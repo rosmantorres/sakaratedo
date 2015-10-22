ba2abc31d2d42d5c1d5706fb29fb9aace43f5e60
adosale <aucab21@gmail.com>
2015-10-22 12:23:07 -0430

    M12_Listado de Competencias
    Se implementó la página que lista las competencias. Faltando registros
    reales y columnas de competencias.

--- GUI/Modulo12/M12_ListarCompetencias.aspx.cs
+++ GUI/Modulo12/M12_ListarCompetencias.aspx.cs
@@ -12,6 +12,18 @@ namespace templateApp.GUI.Modulo12
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "12";
+
+            String success = Request.QueryString["eliminacionSuccess"];
+
+            if (success != null)
+            {
+                if (success.Equals("1"))
+                {
+                    alert.Attributes["class"] = "alert alert-success alert-dismissible";
+                    alert.Attributes["role"] = "alert";
+                    alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Requerimiento agregado exitosamente</div>";
+                }
+            }
         }
     }
 }
