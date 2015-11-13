8c04a3c5f23fd008370593913e71a73267c18893
svillami <sandravillamizar87@gmail.com>
2015-11-13 19:17:55 -0430

    M6_BDInventario
    Se agregaron las regiones de constructores y metodos, los metodos de
    consultarInventario y detallarInventario con su respectiva explicacion y
    se coloco publica la clase de BDInventario.

--- DatosSKD/Modulo16/BDInventario.cs
+++ DatosSKD/Modulo16/BDInventario.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace DatosSKD.Modulo16
 {
-    class BDInventario
+   public class BDInventario
     {
+         #region Constructores
+        /// <summary>
+        /// Constructor vacio de la clase BDInventario
+        /// </summary>
+        public BDInventario()
+        {
+
+        }
+        #endregion
+
+         #region Metodos
+
+        /// <summary>
+        /// Metodo que obtiene todos los productos se que encuentran existentes en el inventario
+        /// </summary>
+        /// <param name=NONE>El metodo no posee parametros particulares</param>
+        /// <returns>Lista con todos los productos existentes en el inventario</returns>
+        /*
+        public inventario consultarInventario()
+        {
+
+        }*/
+
+        /// <summary>
+        /// Metodo que obtiene el datalle de un producto en particular
+        /// </summary>
+        /// <param name="objetoDetallar">el id del objeto a detallar</param>
+        /// /// <param name="idUsuario">el id del usuario que se encuentra en la session</param>
+        /// <returns>objeto de tipo inventario con todos sus detalles</returns>
+        /*
+         public inventario detallarInventario(int objetoDetallar, int idUsuario)
+        {
+
+        }*/
+
+        #endregion
+
     }
 }
