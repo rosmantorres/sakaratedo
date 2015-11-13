6ce6f06ce8646b205ca181e652e1524b0efc00de
Minervavr <minerva_veru@hotmail.com>
2015-11-13 01:33:17 -0430

    M16_SimulacionMetodosNecesarios
    Se generan las clases de prueba para constatar la funcionalidad temprana
    del sistema. para eventos, inventario y matricula

---
+++ LogicaNegociosSKD/Modulo16/Logicaevento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegociosSKD.Modulo16
+{
+    class Logicaevento
+    {
+      /* Evento detallarEvento( int objetoDetallar , int idUsario)
+          {  
+
+            
+           }
+       */
+       
+    }
+}
---
+++ LogicaNegociosSKD/Modulo16/Logicamatricula.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominioSKD;
+using DatosSKD.Modulo16;
+
+namespace LogicaNegociosSKD.Modulo16
+{
+    class Logicamatricula
+    {        
+        #region Metodos
+         /* 
+        /// <summary>
+        /// Metodo que obtiene todos los objetos que estan en el carrito del usuario
+        /// </summary>
+        /// <param name="idUsuario">El usuario el cual se desea ver su carrito</param>
+        /// <returns>Todo lo que tiene actualmente en su carrito</returns>
+      public List<Matricula> mostrarMensualidadesmorosas(int idUsuario)
+        {
+            
+            List<Inventario> listaInventario = carritoBD.getInventario(1);
+            List<Matricula> listaMatricula = carritoBD.getMatricula(1);
+            List<Evento> listaEvento = carritoBD.getEvento(1);
+            return null;
+        }
+ 
+
+
+
+       
+       */
+         #endregion
+    }
+}
