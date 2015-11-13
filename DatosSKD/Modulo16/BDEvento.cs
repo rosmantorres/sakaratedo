0e1d064cab818e7d24df3e3abc7e973bbf5ea795
Minervavr <minerva_veru@hotmail.com>
2015-11-13 01:42:01 -0430

    M16_Simularfuncionalidad
    Se simula la implementacion de los modulos que dependemos
    (eventos,matricula,inventario)

---
+++ DatosSKD/Modulo16/BDEvento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosSKD.Modulo16
+{
+    class BDEvento
+    {
+
+
+
+
+
+        #region Metodos
+        /// <summary>
+        /// Metodo que obtiene todos los items del inventario en el carrito del usuario de la Base de Datos
+        /// </summary>
+        /// <param name="idUsuario">Usuario del que se desea ver los items del iventario agregados en carrito</param>
+        /// <returns>Lista con todos los items del inventario que se encuentran en el carrito</returns>
+        /*
+        public Evento detallarEvento(int objetoDetallar , int idUsario)
+        {
+
+        }*/
+
+
+
+
+        #endregion
+    }
+}
---
+++ DatosSKD/Modulo16/BDMatricula.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosSKD.Modulo16
+{
+    class BDMatricula
+    {
+        /*
+          public List<Matricula> mostrarMensualidadesmorosas(int idUsuario)
+        {
+
+        }
+        */
+
+
+
+    }
+}
