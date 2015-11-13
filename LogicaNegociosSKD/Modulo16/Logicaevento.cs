8a367be1cec14ce69fb97a05b0f3006b7714b07d
svillami <sandravillamizar87@gmail.com>
2015-11-13 19:00:08 -0430

    M16_DetallesLogicaEventos
    Se agregaron las regiones de atributos, constructores, propiedades y
    metodos, se incluyo ademas el metodo consultarEvento con su respectiva
    explicacion.

--- LogicaNegociosSKD/Modulo16/Logicaevento.cs
+++ LogicaNegociosSKD/Modulo16/Logicaevento.cs
@@ -3,17 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DominioSKD;
+using DatosSKD.Modulo16;
 
 namespace LogicaNegociosSKD.Modulo16
 {
     class Logicaevento
     {
-      /* Evento detallarEvento( int objetoDetallar , int idUsario)
+        #region Atributos
+        /// <summary>
+        /// Atributos de la clase Logicacarrito
+        /// </summary>
+        private BDEvento eventoBD;
+        #endregion
+
+        #region Constructores
+        /// <summary>
+        /// Constructor vacio de la clase Logicaevento que inicializa el atributo eventoBD
+        /// </summary>
+        public Logicaevento()
+        {
+            eventoBD = new BDEvento();
+        }
+        #endregion
+
+        #region Propiedades
+
+        #endregion
+
+        #region Metodos
+        /* Evento detallarEvento( int objetoDetallar , int idUsario)
           {  
 
             
            }
        */
-       
+        /// <summary>
+        /// Metodo que obtiene todos los objetos que estan en el inventario de eventos
+        /// </summary>
+        /// <param name=NONE>Este metodo no posee paso de parametros</param>
+        /// <returns>Todo lo que tiene actualmente el inventario de los eventos</returns>
+        /*  public Evento consultarEvento()
+        {         
+            return List<Evento>;
+        }*/
+
+        #endregion
     }
 }
