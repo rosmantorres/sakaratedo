00dd27b7dcd0c6f4c9c04a177631cf8e6a678312
svillami <sandravillamizar87@gmail.com>
2015-11-13 03:22:59 -0430

    M16_LogicaInventario
    Se agregaron las cascaras de los metodos que corresponden a la logica de
    inventario.

--- LogicaNegociosSKD/Modulo16/Logicainventario.cs
+++ LogicaNegociosSKD/Modulo16/Logicainventario.cs
@@ -8,9 +8,42 @@ namespace LogicaNegociosSKD.Modulo16
 {
     class Logicainventario
     {
+        #region Constructores
+        /// <summary>
+        /// Constructor vacio de la clase Logicainventario
+        /// </summary>
+        public Logicainventario()
+        {
 
+        }
+        #endregion
 
+        #region Metodos
+        /*
+        /// <summary>
+        /// Metodo que obtiene todos productos que se encuentran en el inventario
+        /// </summary>
+        /// <returns>Retorna una lista de productos</returns>
+        public List<Inventario> consultarInventario()
+        {
+            return null;
+        }
+        */
 
+        /*
+        /// <summary>
+        /// Metodo que obtiene todos productos que se encuentran en el inventario
+        /// </summary>
+        /// <param name="objetoDetallar">Indica el objeto a detallar</param>
+        /// <param name="idUsuario">Indica el identificador del Usuario que esta </param>
+        /// <returns>Retorna un producto en especifico</returns>
+        public Producto detallarInventario(int objetoDetallar, int idUsuario)
+        {
+            return null;
+        }
+        */
+
+        #endregion
 
     }
 }
