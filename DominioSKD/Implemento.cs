957a171dcaf1c4a597014fb3d7eec6c8cc2b2d45
Armando Hernandez <armando26922@gmail.com>
2015-11-20 10:32:02 -0430

    M15_cambios_clases
    se cambiaron unas clases  de Base de datos

--- DominioSKD/Implemento.cs
+++ DominioSKD/Implemento.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace DominioSKD
 {
-   public  class Implemento
+    public class Implemento
     {
-       
+       private String nombre_implemento;
+       private String tipo_implemento;
+       private String marca_implemento;
+       private String color_implemento;
+       private String talla_implemento;
+       private String stock_minimo_implemento;
 
+       public Implemento() { }
+       public Implemento(String nombre_implemento,String tipo_implemento, String marca_implemento,String color_implemento, String talla_implemento,String stock_implemento)
+       {
+       
+       }
+    
 
     }
 }
