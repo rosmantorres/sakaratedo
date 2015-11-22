051e1ba9ab4d53cf7141499924a94002eed3a68f
svillami <sandravillamizar87@gmail.com>
2015-11-22 10:14:01 -0430

    Revert "M16_ConsultarProductoYLogicaInventario"
    This reverts commit d785d190da1df96fa6e57c6b5adc613a8f8c9b08.

--- GUI/Modulo16/M16_ConsultarProducto.aspx.cs
+++ GUI/Modulo16/M16_ConsultarProducto.aspx.cs
@@ -4,44 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using LogicaNegociosSKD.Modulo16;
-using DominioSKD;
-
 
 namespace templateApp.GUI.Modulo16
 {
     public partial class M16_ConsultarProducto : System.Web.UI.Page
     {
-        private List<Inventario> laLista = new List<Inventario>();
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "16";
-        
-
-        #region Llenar Data Table Con Productos - Inventario
-
-            Logicainventario logComp = new Logicainventario();
-            if (!IsPostBack)
-            {
-                try
-                {
-                    //laLista = logComp.obtenerListaDeInventario();
-
-                    foreach (Inventario c in laLista)
-                    {
-                        /*
-                        //this.laTabla.Text += Recursointerfaz.AbrirTR;
-                        */
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
-            }
         }
-        #endregion
-
     }
 }
--- LogicaNegociosSKD/Modulo16/Logicainventario.cs
+++ LogicaNegociosSKD/Modulo16/Logicainventario.cs
@@ -15,7 +15,6 @@ namespace LogicaNegociosSKD.Modulo16
         /// Atributos de la clase Logicainventario
         /// </summary>
         private BDInventario inventarioBD;
-        private List<DominioSKD.Inventario> laListaDeInventario;
         #endregion
 
         #region Constructores
@@ -29,12 +28,7 @@ namespace LogicaNegociosSKD.Modulo16
         #endregion
 
         #region Propiedades
-        public List<DominioSKD.Inventario> LaListaDeInventario
-        {
-            get { return laListaDeInventario; }
-            set { laListaDeInventario = value; }
-        }
-        
+
         #endregion
 
         #region Metodos
@@ -63,18 +57,6 @@ namespace LogicaNegociosSKD.Modulo16
         }
         */
 
-      /*  public List<DominioSKD.Inventario> obtenerListaDeInventario()
-        {
-            try
-            {
-                return BDInventario.ListarInventario();
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-
-        }*/
         #endregion
 
     }
