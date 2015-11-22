0ce38fb3cde60fab5cfd3006827d059f7bcad405
tjsearsz <tjsearszu@gmail.com>
2015-11-22 15:53:13 -0430

    M16_PruebasUnitarias V1.0
    Se añaden las primeras pruebas correspondientes en tres capas

---
+++ PruebasUnitariasSKD/Modulo16/Pruebacarrito.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominioSKD;
+using NUnit.Framework;
+
+namespace PruebasUnitariasSKD.Modulo16
+{
+    /// <summary>
+    /// Clase de pruebas unitarias que trabaja sobre la clase Carrito
+    /// </summary>
+    /* AUN NO ESTA COMPLETADO */
+    [TestFixture]
+    public class Pruebacarrito
+    {
+        #region Atributos
+        //Atributos con los que trabajara la clase
+        private Carrito carritoPrueba;
+        #endregion
+
+        #region Pruebas Unitarias
+        /// <summary>
+        /// Preparamos los atributos que vamos a utilizar con valores de prueba
+        /// </summary>
+        [SetUp]
+        public void inicializar()
+        {
+            this.carritoPrueba = new Carrito();
+        }
+
+        /// <summary>
+        /// Prueba unitaria que verifica que la clase no sea vacia
+        /// </summary>
+        public void pruebaVacio()
+        {
+            Assert.IsNotNull(this.carritoPrueba);
+        }
+
+        /// <summary>
+        /// Prueba unitaria que verifica que el metodo limpiar funcione
+        /// </summary>
+        public void pruebaLimpiar()
+        {
+            Assert.IsTrue(this.carritoPrueba.limpiar());
+        }
+
+        /// <summary>
+        /// Prueba unitaria que verifica que el metodo eliminarItem funcione
+        /// </summary>
+        public void pruebaEliminaritem()
+        {
+            Assert.IsTrue(this.carritoPrueba.eliminarItem(1,1));
+        }
+
+        /// <summary>
+        /// Limpiamos todos los atributos usados
+        /// </summary>
+        [TearDown]
+        public void limpiar()
+        {
+            this.carritoPrueba = null;
+        }
+        #endregion
+    }
+}
---
+++ PruebasUnitariasSKD/Modulo16/Pruebalogicacarrito.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicaNegociosSKD.Modulo16;
+using NUnit.Framework;
+
+namespace PruebasUnitariasSKD.Modulo16
+{
+    /// <summary>
+    /// Clase de pruebas unitarias que trabaja sobre la clase Logicacarrito
+    /// </summary>
+    /* AUN NO ESTA COMPLETADO */
+    [TestFixture]
+    public class Pruebalogicacarrito
+    {
+        #region Atributos
+        //Atributos con los que trabajara la clase
+        private Logicacarrito logica;
+        #endregion
+
+        #region Pruebas Unitarias
+        /// <summary>
+        /// Preparamos los atributos que vamos a utilizar con valores de prueba
+        /// </summary>
+        [SetUp]
+        public void inicializar()
+        {
+            this.logica = new Logicacarrito();
+        }
+
+        /// <summary>
+        /// Prueba unitaria que verifica que la clase no sea vacia
+        /// </summary>
+        [Test]
+        public void pruebaVacio()
+        {
+            Assert.IsNotNull(this.logica);
+        }
+
+        /// <summary>
+        /// Prueba unitaria que verifica que el metodo eliminarItem funcione
+        /// </summary>
+        [Test]
+        public void pruebaEliminarItem()
+        {
+            Assert.IsTrue(this.logica.eliminarItem(1,1,1));
+        }
+
+        /// <summary>
+        /// Prueba unitaria que verifica que el metodo registrarPago funcione
+        /// </summary>
+        [Test]
+        public void pruebaRegistrarpago()
+        {
+            Assert.IsTrue(this.logica.registrarPago(1,null));
+        }
+
+        /// <summary>
+        /// Limpiamos todos los atributos usados
+        /// </summary>
+        public void limpiar()
+        {
+            this.logica = null;
+        }
+        #endregion
+    }
+}
