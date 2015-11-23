218ec9abb2eb413d3e6a0862aafae659c6f0c6f9
Manuel28G <man281192@gmail.com>
2015-11-22 21:50:53 -0430

    M1_PruebaUHash
    se realizo la prueba unitaria del hash

--- PruebasUnitariasSKD/Modulo1/Prueba.cs
+++ PruebasUnitariasSKD/Modulo1/Prueba.cs
@@ -11,13 +11,33 @@ namespace PruebasUnitariasSKD.Modulo1
     [TestFixture]
     class Prueba
     {
+        [SetUp]
+        protected  void parametros()
+        {
+           
+        }
+      
         [Test]
-        public void Prueba1()
+        public static void Prueba1()
         {
             login lg = new login();
             string[] resp = lg.iniciarSesion("man", "12345");
             Assert.AreEqual(resp, null);
 
+        }
+        [Test]
+        public void PruebaHash()
+        {
+            login lg = new login();
+            Console.WriteLine("hola:" +lg.hash("hola"));
+            Console.WriteLine("hol:" + lg.hash("hol"));
+            Console.WriteLine("1234:" + lg.hash("1234"));
+            Console.WriteLine("1235:" + lg.hash("1235"));
+
+            Assert.AreEqual(lg.hash("hola"), lg.hash("hola"));
+            Assert.AreEqual(lg.hash("1234"), lg.hash("1234"));
+            Assert.AreNotEqual(lg.hash("hol"), lg.hash("hola"));
+            Assert.AreNotEqual(lg.hash("1235"), lg.hash("1234"));
         }
     }
 }
