275f8b6b1fd0c13f7df3caae7635a18c8a974825
gbsojo <gbsojo@gmail.com>
2015-11-22 23:29:49 -0430

    M6_Inserts Persona

---
+++ ScriptsBD/Modulo6/inserts_Persona.sql
@@ -0,0 +1,154 @@
+INSERT INTO dbo.PERSONA (
+    per_tipo_doc_id,
+    per_num_doc_id,
+    per_nombre,
+    per_apellido ,
+    per_nacionalidad,
+    per_alergias,
+    per_direccion,
+    per_sexo,
+    per_tipo_sangre,
+    per_fecha_nacimiento,
+    per_activo,
+    per_peso,
+    per_estatura
+) 
+VALUES (
+    'CEDULA-N',
+    21424696,
+    'Miguel Alejandro',
+    'Ruiz Campos',
+    'Venezolano',
+    'Mariscos',
+    '2da. Av. de Montalban. Res. Capricornio. Piso 5. Apto. 10',
+    'M',
+    'A+',
+    '1993-05-21',
+    1,
+    77,
+    1.72
+);
+
+INSERT INTO dbo.PERSONA (
+    per_tipo_doc_id,
+    per_num_doc_id,
+    per_nombre,
+    per_apellido ,
+    per_nacionalidad,
+    per_alergias,
+    per_direccion,
+    per_sexo,
+    per_tipo_sangre,
+    per_fecha_nacimiento,
+    per_activo,
+    per_peso,
+    per_estatura
+) 
+VALUES (
+    'CEDULA-N',
+    14020474,
+    'Adriana Josefina',
+    'Lopez Sojo',
+    'Venezolana',
+    'Lacteos',
+    '3ra. Transversal de Los Palos Grandes. Res. Guarani. Piso 3. Apto. 3-A',
+    'F',
+    'O-',
+    '1988-11-15',
+    1,
+    63,
+    1.60
+);
+
+INSERT INTO dbo.PERSONA (
+    per_tipo_doc_id,
+    per_num_doc_id,
+    per_nombre,
+    per_apellido ,
+    per_nacionalidad,
+    per_alergias,
+    per_direccion,
+    per_sexo,
+    per_tipo_sangre,
+    per_fecha_nacimiento,
+    per_activo,
+    per_peso,
+    per_estatura
+) 
+VALUES (
+    'CEDULA-E',
+    3253777,
+    'Christian Jose',
+    'Suarez Arraiz',
+    'Colombiano',
+    'Gluten',
+    'Calle Montserrat de Carapita. Quinta Los Reales.',
+    'M',
+    'B+',
+    '1956-08-31',
+    0,
+    83,
+    1.84
+);
+
+INSERT INTO dbo.PERSONA (
+    per_tipo_doc_id,
+    per_num_doc_id,
+    per_nombre,
+    per_apellido ,
+    per_nacionalidad,
+    per_alergias,
+    per_direccion,
+    per_sexo,
+    per_tipo_sangre,
+    per_fecha_nacimiento,
+    per_activo,
+    per_peso,
+    per_estatura
+) 
+VALUES (
+    'CEDULA-N',
+    20532517,
+    'Cesar Augusto',
+    'Rodriguez Torres',
+    'Venezolano',
+    'Penicilina',
+    'Calle principal de Santa Paula. Res. Caroni. Piso 12. PH-B',
+    'M',
+    'AB-',
+    '1979-02-27',
+    1,
+    74,
+    1.75
+);
+
+INSERT INTO dbo.PERSONA (
+    per_tipo_doc_id,
+    per_num_doc_id,
+    per_nombre,
+    per_apellido ,
+    per_nacionalidad,
+    per_alergias,
+    per_direccion,
+    per_sexo,
+    per_tipo_sangre,
+    per_fecha_nacimiento,
+    per_activo,
+    per_peso,
+    per_estatura
+) 
+VALUES (
+    'CEDULA-N',
+    4818349,
+    'Eduardo',
+    'Sanchez Quintero',
+    'Venezolano',
+    'Coco',
+    '2da. Avenida de La Lagunita. Quinta Los Querubines.',
+    'M',
+    'O-',
+    '1968-03-05',
+    1,
+    88,
+    1.78
+);
