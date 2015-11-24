e438845b164e77f5164c29b5ef959dfde70ec093
adrianabetta <adri1191@hotmail.com>
2015-11-24 09:24:04 -0430

    M7_TablaDinamicaPagoAEventos
    Se agrego logica para eventos pagados
    Se modifico GUI ListarPagosAEventos
    Se modifico el datatable de eventos pagos para que se llene
    automaticamente desde un metodo

--- GUI/Modulo7/M7_ListarPagosAEventos.aspx.cs
+++ GUI/Modulo7/M7_ListarPagosAEventos.aspx.cs
@@ -4,14 +4,82 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DominioSKD;
+using LogicaNegociosSKD;
+using LogicaNegociosSKD.Modulo7;
 
 namespace templateApp.GUI.Modulo7
 {
     public partial class M7_ListarPagosAEventos : System.Web.UI.Page
-    {
+    { 
+        #region Atributos
+        private List<Evento> laLista = new List<Evento>();
+        #endregion
+        #region Page Load
+        /// <summary>
+        /// Método que se ejecuta al cargar la página
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "7";
+
+            String detalleString = Request.QueryString["eventDetalle"];
+
+            if (detalleString != null)
+            {
+                llenarModalInfo(int.Parse(detalleString));
+            }
+
+            #region Llenar Data Table con Eventos
+            LogicaEventosPagos logEvento = new LogicaEventosPagos();
+
+            if (!IsPostBack)
+            {
+                try
+                {
+                    laLista = logEvento.obtenerListaDeEventos();
+
+                    foreach (Evento evento in laLista)
+                    {
+                        this.laTabla.Text += M7_Recursos.AbrirTR;
+                        this.laTabla.Text += M7_Recursos.AbrirTD + evento.Id_evento.ToString() + M7_Recursos.CerrarTD;
+                        this.laTabla.Text += M7_Recursos.AbrirTD + evento.Nombre.ToString() + M7_Recursos.CerrarTD;
+                        this.laTabla.Text += M7_Recursos.AbrirTD + evento.TipoEvento.ToString() + M7_Recursos.CerrarTD;
+                        this.laTabla.Text += M7_Recursos.AbrirTD + evento.FechaInicio.ToString() + M7_Recursos.CerrarTD;
+                        this.laTabla.Text += M7_Recursos.AbrirTD + evento.Ubicacion.ToString() + M7_Recursos.CerrarTD;
+                        this.laTabla.Text += M7_Recursos.AbrirTD;
+                        this.laTabla.Text += M7_Recursos.BotonInfoPagosAEventos + evento.Id_evento + M7_Recursos.BotonCerrar;
+                        this.laTabla.Text += M7_Recursos.CerrarTD;
+                        this.laTabla.Text += M7_Recursos.CerrarTR;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
         }
-    }
+        
+            /// <summary>
+            /// Método que llena el modal con la información del evento
+            /// </summary>
+            /// <param name="idEvento">Número entero que representa el ID del evento</param>
+            protected void llenarModalInfo(int idEvento)
+            {
+                Evento evento = new Evento();
+                LogicaEventosPagos logica = new LogicaEventosPagos();
+                evento = logica.detalleEventoID(idEvento);
+            }
+
+        }
+            #endregion
+            
+            
+
+        
+        #endregion
+        
 }
