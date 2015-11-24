c6ae0b91b683cc0d3562eaa0f77a20483ed0a473
svillami <sandravillamizar87@gmail.com>
2015-11-24 17:15:16 -0430

    M16_ConsultarEventoInterfaz
    Se agrega la Interfaz del Consultar Evento.

--- GUI/Modulo16/M16_ConsultarEvento.aspx.cs
+++ GUI/Modulo16/M16_ConsultarEvento.aspx.cs
@@ -4,14 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LogicaNegociosSKD.Modulo16;
+using DominioSKD;
 
 namespace templateApp.GUI.Modulo16
 {
     public partial class M16_ConsultarEvento : System.Web.UI.Page
     {
+        private List<Evento> laLista = new List<Evento>();
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "16";
+
+
+            #region Llenar Data Table Con Evento
+
+            Logicaevento logComp = new Logicaevento();
+            if (!IsPostBack)
+            {
+                try
+                {
+                    laLista = logComp.obtenerListaDeEvento();
+
+                    foreach (Evento c in laLista)
+                    {
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TR;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD + c.Id_evento.ToString() + M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD + c.Nombre.ToString() + M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD + c.Costo.ToString() + M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD;
+
+                        this.laTabla.Text += M16_Recursointerfaz.BOTON_INFO_PRODUCTO + c.Id_evento + M16_Recursointerfaz.BOTON_CERRAR;
+                        this.laTabla.Text += M16_Recursointerfaz.BOTON_INFO + c.Id_evento + M16_Recursointerfaz.BOTON_CERRAR;
+                        this.laTabla.Text += M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.CERRAR_TR;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            #endregion
+
         }
     }
 }
