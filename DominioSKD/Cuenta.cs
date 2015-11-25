d0ff6b015159a6389cd4f28747c3bdeede0ee262
ChrisVidal10 <chris.vidal10@gmail.com>
2015-11-25 17:07:15 -0430

    Metodos BD Login
    Obtiene los atributos necesario para llenar el objeto cuenta

--- DominioSKD/Cuenta.cs
+++ DominioSKD/Cuenta.cs
@@ -9,12 +9,19 @@ namespace DominioSKD
     public class Cuenta
     {
         #region atributos
+        private int id_usuario;
         private String nombre_usuario;
         private String contrasena;
         private List<Rol> roles;
         #endregion
 
         #region propiedades
+        public int Id_usuario
+        {
+            get { return id_usuario; }
+            set { id_usuario = value; }
+        }
+
         public String Nombre_usuario
         {
             get { return nombre_usuario; }
@@ -28,15 +35,23 @@ namespace DominioSKD
         }
         #endregion
 
+        public List<Rol> Roles
+        {
+            get { return roles; }
+            set { roles = value; }
+        }
+
         #region constructores
          public Cuenta()
         {
+            id_usuario = 0;
             nombre_usuario = "";
             contrasena = "";
         }
 
-         public Cuenta(String elNombreUsuario, String laContrasena)
+         public Cuenta(int elIdUsuario, String elNombreUsuario, String laContrasena)
         {
+            id_usuario = elIdUsuario;
             nombre_usuario = elNombreUsuario;
             contrasena = laContrasena;
         }
