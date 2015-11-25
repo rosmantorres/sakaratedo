ad888ca3909e3a02fc10b56303a53be2b5299393
Minervavr <minerva_veru@hotmail.com>
2015-11-25 18:35:04 -0430

    M16_ConsultarMatriculasParteLogica
    Se sube la implementación de la lógica de matricula para poder gestionar
    la muestra de matriculas morosas. Incluyendo la propiedad de Robusto.

--- LogicaNegociosSKD/Modulo16/Logicamatricula.cs
+++ LogicaNegociosSKD/Modulo16/Logicamatricula.cs
@@ -4,33 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DominioSKD;
+using DatosSKD;
 using DatosSKD.Modulo16;
+using ExcepcionesSKD;
+using ExcepcionesSKD.Modulo16.ExcepcionesDeDatos;
+using ExcepcionesSKD.Modulo16;
 
 namespace LogicaNegociosSKD.Modulo16
 {
-    class Logicamatricula
-    {        
-        #region Metodos
-         /* 
-        /// <summary>
-        /// Metodo que obtiene todos los objetos que estan en el carrito del usuario
-        /// </summary>
-        /// <param name="idUsuario">El usuario el cual se desea ver su carrito</param>
-        /// <returns>Todo lo que tiene actualmente en su carrito</returns>
-      public List<Matricula> mostrarMensualidadesmorosas(int idUsuario)
+    public class Logicamatricula
+    {
+
+
+        #region Atributos
+        private int idPersona;
+        private List<DominioSKD.Matricula> laListaDeMatriculas;
+
+        #endregion
+
+
+        #region Get y Set
+        public List<DominioSKD.Matricula> LaListaDeMatriculas
         {
-            
-            List<Inventario> listaInventario = carritoBD.getInventario(1);
-            List<Matricula> listaMatricula = carritoBD.getMatricula(1);
-            List<Evento> listaEvento = carritoBD.getEvento(1);
-            return null;
+            get { return laListaDeMatriculas; }
+            set { laListaDeMatriculas = value; }
         }
- 
+        #endregion
+
+        /// <summary>
+        /// Metodo que obtiene todas las matriculas asociadas a una persona
+        /// </summary>
+        /// <param name="idUsuario">El usuario el cual se desea ver las matriculas en su carrito</param>
+        /// <returns>Todas las matriculas que tiene actualmente el cliente en su carrito</returns>
+        public List<DominioSKD.Matricula> mostrarMensualidadesmorosas(int idPersona)
+        {
+
+            try
+            {
+                return BDMatricula.mostrarMensualidadesmorosas(idPersona);
+            }
 
 
 
-       
-       */
-         #endregion
+            catch (ParametroIncorrectoException ex)
+            {
+                throw new ParametroIncorrectoException(RecursosLogicaModulo16.Codigo_ExcepcionParametro,
+                    RecursosLogicaModulo16.Mensaje__ExcepcionParametro, ex);
+            }
+            catch (AtributoIncorrectoException ex)
+            {
+                throw new AtributoIncorrectoException(RecursosLogicaModulo16.Codigo_ExcepcionAtributo,
+                    RecursosLogicaModulo16.Mensaje_ExcepcionAtributo, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new AtributoIncorrectoException(RecursosLogicaModulo16.Codigo_ExcepcionAtributo,
+                 RecursosLogicaModulo16.Mensaje_ExcepcionAtributo, ex);
+
+            }
+
+
+
+        }
+
+
+
+
+
     }
 }
