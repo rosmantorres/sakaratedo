54c271abfe9d07fb03f9cdf8f85f0bd20bc6a871
Manuel28G <man281192@gmail.com>
2015-11-26 06:41:18 -0430

    ListarRoles
    Se crearon unos recursos y se creo la clase para conectar con el store
    procedure de listarRoles

---
+++ DatosSKD/Modulo2/BDRoles.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominioSKD;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DatosSKD.Modulo1
+{
+    class BDRoles
+    {
+     /*  public static List<Rol> ObtenerUsuario(string nombre_usuario)
+        {
+            BDConexion laConexion;
+            List<Parametro> parametros;
+            Parametro elParametro = new Parametro();
+
+            try
+            {
+                laConexion = new BDConexion();
+                parametros = new List<Parametro>();
+                Cuenta laCuenta = new Cuenta();
+
+                elParametro = new Parametro(RecursosBDModulo1.AliasNombreUsuario, SqlDbType.VarChar, nombre_usuario, false);
+
+
+                parametros.Add(elParametro);
+
+                DataTable dt = laConexion.EjecutarStoredProcedureTuplas(
+                               RecursosBDModulo1.ConsultarNombreUsuarioContrasena, parametros);
+
+                foreach (DataRow row in dt.Rows)
+                {
+
+                    laCuenta.Id_usuario = int.Parse(row[RecursosBDModulo1.AliasIdUsuario].ToString());
+                    laCuenta.Nombre_usuario = row[RecursosBDModulo1.AliasNombreUsuario].ToString();
+                    laCuenta.Contrasena = row[RecursosBDModulo1.AliasContrasena].ToString();
+                }
+
+                DataTable dt1 = laConexion.EjecutarStoredProcedureTuplas(
+                RecursosBDModulo1.ConsultarRolesUsuario, parametros);
+                List<Rol> listaRol = new List<Rol>();
+                foreach (DataRow row in dt1.Rows)
+                {
+
+                    Rol elRol = new Rol();
+                    elRol.Id_rol = int.Parse(row[RecursosBDModulo1.AliasIdRol].ToString());
+                    elRol.Nombre = row[RecursosBDModulo1.AliasNombreRol].ToString();
+                    elRol.Fecha_creacion = (DateTime)row[RecursosBDModulo1.AliasFechaCreacion];
+                    listaRol.Add(elRol);
+                }
+                laCuenta.Roles = listaRol;
+                return laCuenta;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+   
+    */
+    }
+}
