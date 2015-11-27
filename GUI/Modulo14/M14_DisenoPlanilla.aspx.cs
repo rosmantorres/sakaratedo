2b30eb6fb27985152fe6508c17087f3f6e6470a3
vargasmary <mariaisa.v.30@gmail.com>
2015-11-27 12:23:34 -0430

    Arreglo en Diseñar

--- GUI/Modulo14/M14_DisenoPlanilla.aspx.cs
+++ GUI/Modulo14/M14_DisenoPlanilla.aspx.cs
@@ -20,7 +20,8 @@ namespace templateApp.GUI.Modulo14
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "14";
-            if (!IsPostBack)
+            
+            if(!IsPostBack)
             {
                 try
                 {
@@ -31,7 +32,6 @@ namespace templateApp.GUI.Modulo14
                         this.Planilla.Text = Request.Cookies["Planilla"]["nombre"].ToString();
                         if (Request.Cookies["Planilla"]["diseño"].ToString() != "")
                         {
-                            exito = true;
                             dis = logica.ConsultarDiseñoPuro(idPlanilla);
                             CKEditor1.Text = Server.HtmlDecode(dis.Contenido);
                             Request.Cookies["Planilla"].Expires = DateTime.Now;
@@ -42,20 +42,49 @@ namespace templateApp.GUI.Modulo14
                 {
                     string a = exce.Message;
                     exito = false;
+                    this.idPlanilla = 1;
                     this.tipoPlanilla.Text = "Retiro";
                     this.Planilla.Text = "Vacaciones";
                 }
-                planilla1 = new DominioSKD.Planilla(1, this.Planilla.Text, true, this.tipoPlanilla.Text);
-            }
-            
+             }
+            dis = logica.ConsultarDiseñoPuro(1);
+            planilla1 = new DominioSKD.Planilla(1, this.Planilla.Text, true, this.tipoPlanilla.Text);
         }
 
         protected void btnguardar_Click(object sender, EventArgs e)
         {
             htmlEncode = Server.HtmlEncode(CKEditor1.Text);
             DominioSKD.Diseño diseño = new DominioSKD.Diseño(CKEditor1.Text);
-            if (!exito)
+            try
             {
+                if (Request.Cookies["Planilla"]["diseño"].ToString() != "")
+                {
+                   
+                    if (CKEditor1.Text != "")
+                    {
+                        dis.Contenido = this.CKEditor1.Text;
+                        exito = logica.ModificarDiseño(dis);
+
+                        if (exito)
+                        {
+                            alert.Attributes["class"] = "alert alert-success alert-dismissible";
+                            alert.Attributes["role"] = "alert";
+                            alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Diseño de Plantilla Modificado satisfactoriamente</div>";
+                        }
+                        else
+                        {
+                            alert.Attributes["class"] = "alert alert-danger alert-dismissible";
+                            alert.Attributes["role"] = "alert";
+                            alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error no se pudo Modificar el diseño. Intente más tarde</div>";
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception exce)
+            {
+                string a = exce.Message;
                 if (CKEditor1.Text != "")
                 {
                     exito = logica.AgregarDiseño(diseño, planilla1);
@@ -73,29 +102,7 @@ namespace templateApp.GUI.Modulo14
                     }
                 }
             }
-            else
-            {
-                if (CKEditor1.Text != "")
-                {
-                    dis.Contenido = this.CKEditor1.Text;
-                    exito = logica.ModificarDiseño(dis);
-
-                    if (exito)
-                    {
-                        alert.Attributes["class"] = "alert alert-success alert-dismissible";
-                        alert.Attributes["role"] = "alert";
-                        alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Diseño de Plantilla Modificado satisfactoriamente</div>";
-                    }
-                    else
-                    {
-                        alert.Attributes["class"] = "alert alert-danger alert-dismissible";
-                        alert.Attributes["role"] = "alert";
-                        alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Error no se pudo Modificar el diseño. Intente más tarde</div>";
-                    }
-                }
-
-            }
-
         }
+        
     }
 }
