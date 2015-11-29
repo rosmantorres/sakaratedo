36eb6aac69585b293302f9013bb9ad7bbf0b759c
svillami <sandravillamizar87@gmail.com>
2015-11-29 03:09:28 -0430

    M16_LISTARFACTURAS
    Se agregan mas detallaes de Listar Facturas del usuario.

---
+++ GUI/Modulo16/M16_ListarFacturas.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DominioSKD;
+using LogicaNegociosSKD;
+using LogicaNegociosSKD.Modulo16;
+using templateApp.GUI.Master;
+
+namespace templateApp.GUI.Modulo16
+{
+    public partial class M16_ListarFacturas : System.Web.UI.Page
+    {
+        private List<Compra> laLista = new List<Compra>();
+        private int idPersona;
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ((SKD)Page.Master).IdModulo = "16";
+
+            String detalleString = Request.QueryString["impDetalle"];
+
+            if (detalleString != null)
+            {
+               // MetodoM14ListarFacturas(int.Parse(detalleString));
+            }
+
+            #region Llenar Data Table Con Facturas del Usuario 
+
+            Logicacompra logComp = new Logicacompra();
+            if (!IsPostBack)
+            {
+                try
+                {
+                    laLista = logComp.obtenerListaDeCompra(idPersona);
+                    foreach (Compra c in laLista)
+                    {
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TR;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD;
+
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD + c.Com_id.ToString() + M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD + c.Com_tipo_pago.ToString() + M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD + c.Com_fecha_compra.ToString() + M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD + c.Com_estado.ToString() + M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.ABRIR_TD;
+
+                        this.laTabla.Text += M16_Recursointerfaz.BOTON_INFO_PRODUCTO + c.Com_id + M16_Recursointerfaz.BOTON_CERRAR;
+                        this.laTabla.Text += M16_Recursointerfaz.CERRAR_TD;
+                        this.laTabla.Text += M16_Recursointerfaz.CERRAR_TR;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+            #endregion
+
+        
+    }
+}
--- LogicaNegociosSKD/Modulo16/Logicacompra.cs
+++ LogicaNegociosSKD/Modulo16/Logicacompra.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DominioSKD;
+using DatosSKD.Modulo16;
 
 namespace LogicaNegociosSKD.Modulo16
 {
@@ -12,22 +13,48 @@ namespace LogicaNegociosSKD.Modulo16
     /// </summary>
     public class Logicacompra
     {
+        #region Atributos
+        /// <summary>
+        /// Atributos de la clase Logicainventario
+        /// </summary>
+        private BDCompra compraBD;
+        private int idPersona;
+        private List<DominioSKD.Compra> laListaDeCompra;
+
+        #endregion
+
+
         #region Constructores
         /// <summary>
-        /// Constructor vacio de la clase Logicacompra
+        /// Constructor que devuelve una lista de tipo compra
         /// </summary>
         public Logicacompra()
         {
-
+            laListaDeCompra = obtenerListaDeCompra(idPersona);
         }
         #endregion
 
+
         #region Metodos
 
-        public List<Dictionary<int,int>> ActualizarInventario() 
+        /// <summary>
+        /// Metodo que obtiene todas las facturas del usuario conectado
+        /// </summary>
+        /// <param name=NONE>Este metodo no posee paso de parametros</param>
+        /// <returns>Retorna una lista de facturas</returns>
+        public List<DominioSKD.Compra> obtenerListaDeCompra(int idPersona)
         {
-                throw new NotImplementedException;
+            try
+            {
+                return BDCompra.ListarCompra(idPersona);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
         }
+     
         #endregion
 
 
