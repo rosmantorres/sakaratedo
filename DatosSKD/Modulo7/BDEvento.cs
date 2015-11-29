f2a7d409a47a397757e4af07d2e15d60ce261087
Pedro Villarreal <pedrovillarreal@gmail.com>
2015-11-28 19:40:22 -0430

    M7_AgregacionListarCompetencia
    Se agregó metodo para listar competencias asistidas, metodo para
    detallar evento

--- DatosSKD/Modulo7/BDEvento.cs
+++ DatosSKD/Modulo7/BDEvento.cs
@@ -20,15 +20,6 @@ namespace DatosSKD.Modulo7
         /// <returns>Lista de eventos</returns>
         public static List<Evento> ListarEventosAsistidos()
         {
-            /*Int16 id = 1;
-            String nombre = "Seminario Cintas Negras";
-            List<Evento> laListaDeEventoAsistidos = new List<Evento>();
-            TipoEvento tipoEvento = BDTipoEvento.DetallarTipoEvento(1);
-            Ubicacion ubicacion = BDUbicacion.DetallarUbicacion(1);
-            Horario horario = BDHorario.DetallarHorario(1);
-            Evento evento = new Evento(id, nombre, "desc:primer evento", 200, ubicacion, tipoEvento, horario);
-            laListaDeEventoAsistidos.Add(evento);*/
-
             int idPersona = 1;
             BDConexion laConexion;
             List<Parametro> parametros;
@@ -80,9 +71,64 @@ namespace DatosSKD.Modulo7
             {
                 throw new ExcepcionesSKD.ExceptionSKD("No se pudo completar la operacion", ex);
             }
+        }
+
+        /// <summary>
+        /// Método para listar las competencias asistidas del atleta
+        /// </summary>
+        /// <returns>Lista de competencias</returns>
+        public static List<Competencia> ListarCompetenciasAsistidas()
+        {
+            int idPersona = 1;
+            BDConexion laConexion;
+            List<Parametro> parametros;
+            Parametro elParametro = new Parametro();
+            List<Competencia> laListaDeCompetenciasAsistidas = new List<Competencia>();
+
+            try
+            {
+                laConexion = new BDConexion();
+                parametros = new List<Parametro>();
 
 
-            //return laListaDeEventoAsistidos;
+                elParametro = new Parametro(RecursosBDModulo7.ParamIdPersona, SqlDbType.Int, idPersona.ToString(), false);
+                parametros.Add(elParametro);
+
+                DataTable dt = laConexion.EjecutarStoredProcedureTuplas(
+                               RecursosBDModulo7.ConsultarCompetenciasAsistidas, parametros);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    Competencia competencia = new Competencia();
+                    competencia.Id_competencia = int.Parse(row[RecursosBDModulo7.AliasIdCompetencia].ToString());
+                    competencia.Nombre = row[RecursosBDModulo7.AliasCompetenciaNombre].ToString();
+                    competencia.FechaInicio = DateTime.Parse(row[RecursosBDModulo7.AliasCompetenciaFechaInicio].ToString());
+                    competencia.FechaFin = DateTime.Parse(row[RecursosBDModulo7.AliasCompetenciaFechaInicio].ToString());
+                    competencia.Costo = int.Parse(row[RecursosBDModulo7.AliasCompetenciaCosto].ToString());
+                    competencia.Ubicacion = BDUbicacion.DetallarUbicacion(int.Parse(row[RecursosBDModulo7.AliasCompetenciaUbicacionId].ToString()));                    
+                    laListaDeCompetenciasAsistidas.Add(competencia);
+                }
+
+                return laListaDeCompetenciasAsistidas;
+
+            }
+            catch (SqlException ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }/*
+            catch (ExcepcionesSKD.Modulo12.CompetenciaInexistenteException ex)
+            {
+                throw ex;
+            }*/
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKD("No se pudo completar la operacion", ex);
+            }
         }
 
         /// <summary>
@@ -185,14 +231,58 @@ namespace DatosSKD.Modulo7
         /// <returns>Objeto de tipo Evento</returns>
         public static Evento DetallarEvento(int idEvento)
         {
-            Int16 id = 1;
-            String nombre = "Seminario Cintas Negras"; 
-            TipoEvento tipoEvento = BDTipoEvento.DetallarTipoEvento(1);
-            Ubicacion ubicacion = BDUbicacion.DetallarUbicacion(1);
-            Horario horario = BDHorario.DetallarHorario(1);
-            Evento evento = new Evento(id, nombre, "desc:primer evento", 200, ubicacion, tipoEvento, horario);
+            
+            BDConexion laConexion;
+            List<Parametro> parametros;
+            Parametro elParametro = new Parametro();
 
-            return evento;
+            try
+            {
+                laConexion = new BDConexion();
+                parametros = new List<Parametro>();
+                Evento evento = new Evento();
+
+                elParametro = new Parametro(RecursosBDModulo7.ParamIdEvento, SqlDbType.Int, idEvento.ToString(), false);
+                parametros.Add(elParametro);
+
+                DataTable dt = laConexion.EjecutarStoredProcedureTuplas(
+                               RecursosBDModulo7.ConsultarEventoXId, parametros);
+
+                foreach (DataRow row in dt.Rows)
+                {
+
+                    evento.Id_evento = int.Parse(row[RecursosBDModulo7.AliasIdEvento].ToString());
+                    evento.Nombre = row[RecursosBDModulo7.AliasEventoNombre].ToString();
+                    evento.Descripcion = row[RecursosBDModulo7.AliasDescripcionEvento].ToString();
+                    evento.Costo = float.Parse(row[RecursosBDModulo7.AliasEventoCosto].ToString());
+                    evento.TipoEvento = BDTipoEvento.DetallarTipoEvento(int.Parse(row[RecursosBDModulo7.AliasEventoTipoEveId].ToString()));
+                    evento.Horario = BDHorario.DetallarHorario(int.Parse(row[RecursosBDModulo7.AliasEventoHorarioId].ToString()));
+                    evento.Ubicacion = BDUbicacion.DetallarUbicacion(int.Parse(row[RecursosBDModulo7.AliasEventoUbicacionId].ToString()));
+
+                }
+
+                return evento;
+
+            }
+            catch (SqlException ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }/*
+            catch (ExcepcionesSKD.Modulo12.CompetenciaInexistenteException ex)
+            {
+                throw ex;
+            }*/
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKD("No se pudo completar la operacion", ex);
+            }
+
+            
         }
 
 
