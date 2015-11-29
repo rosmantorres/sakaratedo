45a62274c2ea73c5d9ebb5af621abb32121703e7
Boris Treccani <bps139@gmail.com>
2015-11-29 13:44:08 -0430

    modified:   DatosSKD/Modulo8/BDRestriccionCompetencia.cs modified:   DatosSKD/Modulo8/RecursosBDModulo8.Designer.cs modified:   DatosSKD/Modulo8/RecursosBDModulo8.resx copied:     DatosSKD/Modulo8/RecursosBDModulo8.Designer.cs -> DatosSKD/Modulo8/RecursosBDRestriccionCompetencia.Designer.cs copied:     DatosSKD/Modulo8/RecursosBDModulo8.resx -> DatosSKD/Modulo8/RecursosBDRestriccionCompetencia.resx modified:   DominioSKD/RestriccionCompetencia.cs

--- DatosSKD/Modulo8/BDRestriccionCompetencia.cs
+++ DatosSKD/Modulo8/BDRestriccionCompetencia.cs
@@ -3,10 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+using DominioSKD;
+using ExcepcionesSKD;
+using System.Globalization;
 
 namespace DatosSKD.Modulo8
 {
     class BDRestriccionCompetencia
     {
+        #region ListarRestriccionesCompetencia
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static List<RestriccionCompetencia> ListarRestriccionesCompetencia()
+        {
+            BDConexion laConexion;
+            List<RestriccionCompetencia> listaDeRestriccionesCompetencia = new List<RestriccionCompetencia>();
+            List<Parametro> parametros;
+
+            try
+            {
+                laConexion = new BDConexion();
+                parametros = new List<Parametro>();
+
+
+                DataTable dt = laConexion.EjecutarStoredProcedureTuplas(
+                               RecursosBDRestriccionCompetencia.ConsultarTodasRestriccionCompetencia, parametros);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    RestriccionCompetencia restriccionDeCompetencia = new RestriccionCompetencia();
+
+                    restriccionDeCompetencia.IdRestriccionComp = int.Parse(row[RecursosBDRestriccionCompetencia.AliasIdRestriccionCompetencia].ToString());
+                    restriccionDeCompetencia.Descripcion = row[RecursosBDRestriccionCompetencia.AliasDescripcion].ToString();
+                    restriccionDeCompetencia.EdadMinima = int.Parse(row[RecursosBDRestriccionCompetencia.AliasEdadMin].ToString());
+                    restriccionDeCompetencia.EdadMaxima = int.Parse(row[RecursosBDRestriccionCompetencia.AliasEdadMax].ToString());
+                    restriccionDeCompetencia.Sexo = row[RecursosBDRestriccionCompetencia.AliasSexo].ToString();
+                    restriccionDeCompetencia.Modalidad = row[RecursosBDRestriccionCompetencia.AliasModalidad].ToString();
+
+                    listaDeRestriccionesCompetencia.Add(restriccionDeCompetencia);
+
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKD(RecursoGeneralBD.Mensaje_Generico_Error, ex);
+            }
+
+            return listaDeRestriccionesCompetencia;
+        } 
+        #endregion
     }
 }
--- DominioSKD/RestriccionCompetencia.cs
+++ DominioSKD/RestriccionCompetencia.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DominioSKD
 {
-    class RestriccionCompetencia
+    public class RestriccionCompetencia
     {
         #region Atributos
         private int idRestriccionComp;
@@ -15,7 +15,7 @@ namespace DominioSKD
         private int edadMaxima;
         private DateTime fechaEdadMin;
         private DateTime fechaEdadMax;
-        private char sexo;
+        private String sexo;
         private String modalidad;
         private List<Competencia> listaCompetencias;
         private List<Cinta> listaCintas;
@@ -59,7 +59,7 @@ namespace DominioSKD
             set { fechaEdadMax = value; }
         }
 
-        public char Sexo
+        public String Sexo
         {
             get { return sexo; }
             set { sexo = value; }
@@ -94,13 +94,13 @@ namespace DominioSKD
             edadMaxima = 0;
             fechaEdadMin = new DateTime();
             fechaEdadMax = new DateTime();
-            sexo = new char();
+            sexo = String.Empty;
             modalidad = String.Empty;
             listaCompetencias = null;
             listaCintas = null;
         }
 
-        public RestriccionCompetencia(int inputId, String inputDescripcion, int inputEdadMinima, int inputEdadMaxima, DateTime inputFechaEdadMin, DateTime inputFechaEdadMax, char inputSexo, String inputModalidad)
+        public RestriccionCompetencia(int inputId, String inputDescripcion, int inputEdadMinima, int inputEdadMaxima, DateTime inputFechaEdadMin, DateTime inputFechaEdadMax, String inputSexo, String inputModalidad)
         {
             idRestriccionComp = inputId;
             descripcion = inputDescripcion;
