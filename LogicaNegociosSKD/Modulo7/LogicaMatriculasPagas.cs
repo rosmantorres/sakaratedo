16e532905698a06a5b650683df74ac6372a8c2ca
adrianabetta <adri1191@hotmail.com>
2015-11-28 22:28:06 -0430

    M7_ModificacionRecurso
    Se modifico recursosBDModulo7

---
+++ LogicaNegociosSKD/Modulo7/LogicaMatriculasPagas.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosSKD;
+using DatosSKD.Modulo7;
+
+namespace LogicaNegociosSKD.Modulo7
+{
+    /// <summary>
+    /// Clase para obtener la lista de matriculas pagadas y la descripción de matricula
+    /// </summary>
+    public class LogicaMatriculasPagas
+    {
+        #region Atributos
+        private List<DominioSKD.Matricula> laListaDeMatriculas;
+        #endregion
+
+        #region Get y Set
+        public List<DominioSKD.Matricula> LaListaDeEventos
+        {
+            get { return laListaDeMatriculas; }
+            set { laListaDeMatriculas = value; }
+        }
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LogicaMatriculasPagas()
+        {
+            laListaDeMatriculas = obtenerListaDeMatriculas();
+        }
+
+        /// <summary>
+        /// Método que obtiene la lista de matriculas que han sido pagadas
+        /// </summary>
+        /// <returns>Lista de objetos tipo Matricula</returns>
+        public List<DominioSKD.Matricula> obtenerListaDeMatriculas()
+        {
+            try
+            {
+                return BDMatricula.ListarMatriculasPagas();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Método que obtiene el detalle de cada matricula por su ID
+        /// </summary>
+        /// <param name="idEvento">Número entero que representa el ID de la matricula</param>
+        /// <returns>un objeto de tipo Matricula</returns>
+        public DominioSKD.Matricula detalleMatriculaID(int idMatricula)
+        {
+            try
+            {
+                return BDMatricula.DetallarMatricula(idMatricula);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        #endregion
+
+    }
+}
