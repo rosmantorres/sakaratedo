f1fbbce89dcf0e4f02d804b36934a934609d3a11
roseregnault <rgomezregnault68@gmail.com>
2015-11-30 01:14:26 -0430

    CommitFinalReporte
    CommitFinalReporteListaAtleta

--- GUI/Modulo13/M13_KyuPrograma.aspx.cs
+++ GUI/Modulo13/M13_KyuPrograma.aspx.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DominioSKD;
+using LogicaNegociosSKD;
+using LogicaNegociosSKD.Modulo13;
+
+
+
 
 namespace templateApp.GUI.Modulo13
 {
@@ -12,6 +18,33 @@ namespace templateApp.GUI.Modulo13
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "13";
+
+
+            try
+            {
+                
+                List<Persona> laLista;
+                LogicaAtletaCinta logCinta = new LogicaAtletaCinta();
+                laLista = logCinta.obtenerListaPersona();
+                 
+                foreach (DominioSKD.Persona valor in laLista  )
+                {
+                    Response.Write("<tr>");
+                    Response.Write("<td>" + valor.Nombre + "</td>");
+                    Response.Write("<td>" + valor.Apellido + "</td>");
+                    Response.Write("<td>" + valor.Edad + "</td>");
+                    Response.Write("<td>" + valor.Peso + "</td>");
+                    Response.Write("<td>" + valor.Estatura + "</td>");
+
+                }
+
+            }
+             catch(Exception ex)
+             {
+                 throw ex;
+             }
+
+
         }
     }
 }
