faaa988dd8c8723a22b24f725ab597b2bc32a680
leojma <leojma@gmail.com>
2015-11-30 01:47:02 -0430

    informacion de inventario
    informacion de inventario 50%

---
+++ LogicaNegociosSKD/Modulo13/LogicaInformacionProductos.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominioSKD;
+using DatosSKD;
+namespace LogicaNegociosSKD.Modulo13
+{
+     class LogicaInformacionProducto
+    {
+
+        #region
+        private List<Implemento> listaImplementos;
+        #endregion
+
+         #region constructor
+        public LogicaInformacionProducto()
+        {
+            this.listaImplementos = null;
+        }
+        #endregion
+
+
+        #region listarimplementos
+
+        public List<Implemento> listarimplementos()
+        {
+
+            try
+            {
+                listaImplementos = ConexionBaseDatos.listarInventarioDatos(dojo);
+            }
+
+            catch (ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+
+            catch (ErrorEnParametroDeProcedure ex)
+            {
+
+                throw ex;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return listaImplementos;
+        }
+        
+        
+        #endregion
+
+    }
+}
