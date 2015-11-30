69bac08150da4e4d7c3b3c72719eb56a3d82bd82
roseregnault <rgomezregnault68@gmail.com>
2015-11-29 22:16:33 -0430

    FinalDatos
    Final datosskd reporte listar atletas por cinta

--- DatosSKD/Modulo13/ListarAtletasCinta.cs
+++ DatosSKD/Modulo13/ListarAtletasCinta.cs
@@ -34,17 +34,36 @@ namespace DatosSKD.Modulo13
 
                 foreach (DataRow row in dt.Rows)
                 {
-                    Persona personas = new Persona;
-                    
-                    
+                    Persona personas = new Persona();
+                    personas.Apellido = row[RecursosBDModulo13.apellido].ToString();
+                    personas.Nombre = row[RecursosBDModulo13.nombre].ToString();
+                    personas.Estatura = double.Parse(row[RecursosBDModulo13.estatura].ToString());
+                    personas.Peso = double.Parse(row[RecursosBDModulo13.peso].ToString());
+                    personas.FechaNacimiento = DateTime.Parse(row[RecursosBDModulo13.Edad].ToString());
+
+                    persona.Add(personas);
+
 
                 }
-            
-            
-            
-            
+
+                return persona;
+
             }
 
+
+            catch (SqlException ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKDConexionBD(RecursoGeneralBD.Codigo, RecursoGeneralBD.Mensaje, ex);
+            }
+
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new ExcepcionesSKD.ExceptionSKD("No se pudo completar la operacion", ex);
+            }
 
 
         }
