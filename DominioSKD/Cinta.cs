31c54b25868bbe8f5592c1f288365aed8ca68b15
yunethm26 <yunethmolina2603@gmail.com>
2015-12-01 00:10:51 -0430

    Modificaciones M5 (Dominio, Datos y Logica)

--- DominioSKD/Cinta.cs
+++ DominioSKD/Cinta.cs
@@ -6,73 +6,121 @@ using System.Threading.Tasks;
 
 namespace DominioSKD
 {
-    class Cinta
+    public class Cinta
     {
+
         private int id_cinta;
         private String color_nombre;
-        private String rango ;
+        private String rango;
         private String clasificacion;
         private int orden;
+        private String significado;
         private int id_restriccion;
+        private Organizacion organizacion;
+
         public int Id_cinta
         {
             get { return id_cinta; }
             set { id_cinta = value; }
         }
-        
+
         public String Color_nombre
         {
             get { return color_nombre; }
             set { color_nombre = value; }
         }
-         public String Rango
+
+        public String Rango
         {
             get { return rango; }
             set { rango = value; }
         }
-          public String Clasificacion
+
+        public String Clasificacion
         {
             get { return clasificacion; }
             set { clasificacion = value; }
         }
-           public int Orden
+
+        public int Orden
         {
             get { return orden; }
             set { orden = value; }
         }
-            public int Id_restriccion
+
+        public String Significado
+        {
+            get { return significado; }
+            set { significado = value; }
+        }
+
+        public int Id_restriccion
         {
             get { return id_restriccion; }
             set { id_restriccion = value; }
         }
+        public Organizacion Organizacion
+        {
+            get { return organizacion; }
+            set { organizacion = value; }
+        }  
+
         public Cinta()
         {
             id_cinta = 0;
             color_nombre = "";
-            rango="";
-            clasificacion="";
-            orden=0;
-            id_restriccion=0;
+            rango = "";
+            clasificacion = "";
+            orden = 0;
+            significado = "";
+            id_restriccion = 0;
+            organizacion = null;
         }
 
-        public Cinta(int elId, String elColor, String elRango, String laClasificacion, int elOrden,int elIdRestriccion)
+        public Cinta(int elId, String elColor, String elRango, String laClasificacion, int elOrden, String elSignificado, int elIdRestriccion)
         {
             id_cinta = elId;
-            color_nombre = elColor;
-            rango=elRango;
-            clasificacion=laClasificacion;
-            orden=elOrden;
-            id_restriccion = elIdRestriccion;
-        }
-
-        public Cinta( String elColor, String elRango, String laClasificacion, int elOrden, int elIdRestriccion)
-        {
-            
             color_nombre = elColor;
             rango = elRango;
             clasificacion = laClasificacion;
             orden = elOrden;
+            significado = elSignificado;
             id_restriccion = elIdRestriccion;
+
+        }
+
+        public Cinta(String elColor, String elRango, String laClasificacion, int elOrden, String elSignificado, int elIdRestriccion)
+        {
+            color_nombre = elColor;
+            rango = elRango;
+            clasificacion = laClasificacion;
+            orden = elOrden;
+            significado = elSignificado;
+            id_restriccion = elIdRestriccion;
+
+        }
+        //De M5
+        public Cinta(int elId,String elColor, String elRango, String laClasificacion, int elOrden, String elSignificado, Organizacion organizacion)
+        {
+            id_cinta = elId;
+            color_nombre = elColor;
+            rango = elRango;
+            clasificacion = laClasificacion;
+            orden = elOrden;
+            significado = elSignificado;
+            organizacion = null;
+
+        }
+
+        public Cinta(String elColor, String elRango, String laClasificacion, int elOrden, String elSignificado, Organizacion organizacion)
+        {
+            color_nombre = elColor;
+            rango = elRango;
+            clasificacion = laClasificacion;
+            orden = elOrden;
+            significado = elSignificado;
+            organizacion = null;
+
         }
 
 
---
+++ LogicaNegociosSKD/Modulo5/LogicaCinta.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosSKD;
+using DatosSKD.Modulo5;
+using ExcepcionesSKD;
+
+namespace LogicaNegociosSKD.Modulo5
+{
+    public class LogicaCinta
+    {
+        #region Atributos
+        private List<DominioSKD.Cinta> ListaDeCintas;
+        #endregion
+
+        #region Get y Set
+        public List<DominioSKD.Cinta> listaDeCintas
+        {
+            get { return ListaDeCintas; }
+            set { ListaDeCintas = value; }
+        }
+        #endregion
+
+        #region constructores
+          public LogicaCinta()
+        {
+        }
+        #endregion
+
+        #region metodos
+        public List<DominioSKD.Cinta> obtenerListaDeCinta()
+          {
+              try
+              {
+
+                  return BDCinta.ListarCintas();
+              }
+              catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+              {
+                  throw ex;
+              }
+              catch (ExcepcionesSKD.Modulo5.FormatoIncorrectoException ex)
+              {
+                 // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                  throw ex;
+              }
+              catch (ExcepcionesSKD.ExceptionSKD ex)
+              {
+                 // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                  throw ex;
+              }
+
+          }
+
+        public List<DominioSKD.Cinta> obtenerListaDeCintaXOrganizacion(int idOrganizacion)
+        {
+            try
+            {
+
+                return BDCinta.ListarCintasXOrganizacion(idOrganizacion);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+            catch (ExcepcionesSKD.Modulo5.FormatoIncorrectoException ex)
+            {
+                // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.ExceptionSKD ex)
+            {
+                // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+
+        }
+
+        public DominioSKD.Cinta detalleDeCinta(int idCinta)
+        {
+            try
+            {
+
+                return BDCinta.DetallarCinta(idCinta);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+            catch (ExcepcionesSKD.Modulo5.FormatoIncorrectoException ex)
+            {
+                // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.ExceptionSKD ex)
+            {
+                // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+
+        }
+
+        public bool agregarCompetencia(DominioSKD.Cinta laCinta)
+        {
+           // Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosLogicaModulo12.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            try
+            {
+
+              //  Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosLogicaModulo12.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+                return BDCinta.AgregarCinta(laCinta);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+              //  Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+             catch (ExcepcionesSKD.Modulo5.FormatoIncorrectoException ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.ExceptionSKD ex)
+            {
+               // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+        }
+
+        public bool modificarCompetencia(DominioSKD.Cinta laCinta)
+        {
+            // Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosLogicaModulo12.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            try
+            {
+
+                //  Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosLogicaModulo12.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+                return BDCinta.ModificarCinta(laCinta);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                //  Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.Modulo5.FormatoIncorrectoException ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.ExceptionSKD ex)
+            {
+                // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+        }
+
+        public bool eliminarCompetencia(int idCinta)
+        {
+            // Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosLogicaModulo12.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            try
+            {
+
+                //  Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosLogicaModulo12.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+                return BDCinta.EliminarCinta(idCinta);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                //  Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.Modulo5.FormatoIncorrectoException ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.ExceptionSKD ex)
+            {
+                // Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+        }
+        #endregion
+
+    }
+}
