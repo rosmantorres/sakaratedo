d87df7e365bb57d60f2f46117bfe5b39705806cd
melaniegodoy <melanie.g1257@gmail.com>
2015-12-02 12:38:19 -0430

    M7_PruebasUnitariasLogica
    Se realizan pruebas a Logica Organizacion y Dojo

---
+++ PruebasUnitariasSKD/Modulo7/M7_PruebasLogicaHorarioPractica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DominioSKD;
+using ExcepcionesSKD.Modulo7;
+using ExcepcionesSKD;
+using LogicaNegociosSKD.Modulo7;
+using DatosSKD.Modulo7;
+
+namespace PruebasUnitariasSKD.Modulo7
+{
+    class M7_PruebasLogicaHorarioPractica
+    {
+    }
+}
