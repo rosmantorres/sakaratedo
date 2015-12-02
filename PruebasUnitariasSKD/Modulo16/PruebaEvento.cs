14f0c5b8bbb389c071a620a30aa35b4060282fe6
Minervavr <minerva_veru@hotmail.com>
2015-12-02 06:30:10 -0430

    M16_PruebasUnitarias
    Se implementan las pruebas del modulo 16.

---
+++ PruebasUnitariasSKD/Modulo16/PruebaEvento.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DominioSKD;
+using NUnit.Framework;
+
+
+namespace PruebasUnitariasSKD.Modulo16
+{
+    /// <summary>
+    /// Prueba Unitaria que trabaja sobre la entidad Carrito
+    /// </summary>
+    [TestFixture]
+    public class PruebaEvento
+    {
+        //Los diferentes usuarios sobre los que probaremos.
+
+        private Evento miEvento;
+        private TipoEvento tipeven;
+        private Horario horario;
+        private Ubicacion ubicacion;
+
+        /// <summary>
+        /// Inicializa las clases que probaremos
+        /// </summary>
+        [SetUp]
+        public void Init()
+        {
+            Evento miEvento = new Evento();
+            miEvento.Id_evento = 1;
+            miEvento.Nombre = "Clase Regular";
+            TipoEvento tipeven = new TipoEvento();
+            tipeven.Id = 4;
+            tipeven.Nombre = "Clases Regulares";
+            miEvento.TipoEvento = tipeven;
+            Horario horario = new Horario();
+            horario.FechaFin = Convert.ToDateTime(15 / 10 / 2016);
+            horario.FechaInicio = Convert.ToDateTime(15 / 10 / 2015);
+            horario.Id = 1;
+            horario.HoraFin = 4;
+            horario.HoraInicio = 2;
+            miEvento.Horario = horario;
+            miEvento.Costo = 0;
+            miEvento.Categoria = null;
+            Ubicacion ubicacion = new Ubicacion();
+            ubicacion.Ciudad = "Caracas";
+            ubicacion.Direccion = null;
+            ubicacion.Id_ubicacion = 1;
+            ubicacion.Latitud = "10.499607";
+            ubicacion.Longitud = "-66.788419";
+            miEvento.Ubicacion = ubicacion;
+
+
+        }
+
+        /// <summary>
+        /// Se prueba que los diferentes Usuarios no apunten a vacio
+        /// </summary>
+        [Test]
+        public void PruebaVacio()
+        {
+            Assert.IsNotNull(miEvento);
+
+
+        }
+
+        /// <summary>
+        /// Se prueba que los diferentes actores tengan los valores correspondientes
+        /// </summary>
+        [Test]
+        public void PruebaValores()
+        {
+            //Verificamos todos los valores del carrito completo con respecto a el evento contenido
+            Assert.AreEqual(1, miEvento.Id_evento);
+            Assert.AreEqual("Clase Regular", miEvento.Nombre);
+            Assert.AreEqual("clave", miEvento.TipoEvento);
+            Assert.AreEqual(0, miEvento.Costo);
+            Assert.AreEqual(4, tipeven.Id);
+            Assert.AreEqual("Clases Regulares", tipeven.Nombre);
+            Assert.AreEqual(1, horario.Id);
+            Assert.AreEqual(4, horario.HoraFin);
+            Assert.AreEqual(2, horario.HoraInicio);
+            Assert.AreEqual("Caracas", ubicacion.Ciudad);
+            Assert.AreEqual(null, ubicacion.Direccion);
+            Assert.AreEqual(1, ubicacion.Id_ubicacion);
+            Assert.AreEqual("10.499607", ubicacion.Latitud);
+            Assert.AreEqual("-66.788419", ubicacion.Longitud);
+
+
+
+        }
+
+
+
+        /// <summary>
+        /// Se deja en vacio los atributos creados para ser limpiados por el Garbage Collector
+        /// </summary>
+        [TearDown]
+        public void Limpiar()
+        {
+
+            miEvento = null; ;
+
+        }
+    }
+}
---
+++ PruebasUnitariasSKD/Modulo16/PruebalogicaMatricula.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominioSKD;
+using NUnit.Framework;
+using LogicaNegociosSKD.Modulo16;
+using LogicaNegociosSKD;
+
+namespace PruebasUnitariasSKD.Modulo16
+{
+    /// <summary>
+    /// Clase de pruebas unitarias que trabaja sobre la clase Carrito
+    /// </summary>
+    /* AUN NO ESTA COMPLETADO */
+    [TestFixture]
+    public class PruebalogicaMatricula
+    {
+        #region Atributos
+        //Atributos con los que trabajara la clase
+
+        private List<DominioSKD.Matricula> l;
+        #endregion
+
+        #region Pruebas Unitarias
+        /// <summary>
+        /// Preparamos los atributos que vamos a utilizar con valores de prueba
+        /// </summary>
+        [SetUp]
+        public void inicializar()
+        {
+            List<DominioSKD.Matricula> l = new List<DominioSKD.Matricula>();
+
+        }
+
+        /// <summary>
+        /// Prueba unitaria que verifica que se obtiene la lista de compra
+        /// </summary>
+        [Test]
+        public void pruebaobtenerListaMatriculas()
+        {
+            l =Logicamatricula.mostrarMensualidadesmorosas(1);
+            /// Prueba unitaria que verifica que el objeto que instancia a logicaComra no es vacia
+            Assert.IsNotNull(l);
+
+            /// Prueba unitaria que verifica que obtener lista de compra es igual que el otro objeto que se compara
+
+            
+            Matricula laMatricula = new Matricula();
+            laMatricula.Mat_identificador = "CAF - CAF - CAFE";
+            laMatricula.Status = true;
+            laMatricula.FechaCreacion = Convert.ToDateTime(29 / 11 / 2015);
+            laMatricula.UltimaFechapago = Convert.ToDateTime(29 / 11 / 2015);
+            Persona persona = new Persona();
+            persona.ID = 1;
+            laMatricula.IdentificadorUsuario = persona.ID;
+            laMatricula.FechaTope = Convert.ToDateTime(29 / 11 / 2015);
+            TipoEvento tipeven = new TipoEvento();
+
+             Matricula laMatricula2 = new Matricula();
+            laMatricula2.Mat_identificador = "CAF - CAF - CAFE";
+            laMatricula2.Status = true;
+            laMatricula2.FechaCreacion = Convert.ToDateTime(29 / 11 / 2015);
+            laMatricula2.UltimaFechapago = Convert.ToDateTime(29 / 11 / 2015);
+            Persona persona2 = new Persona();
+            persona2.ID = 1;
+            laMatricula2.IdentificadorUsuario = persona.ID;
+            laMatricula2.FechaTope = Convert.ToDateTime(29 / 11 / 2015);
+            TipoEvento tipeven2 = new TipoEvento();
+
+
+
+            l.Add(laMatricula);
+
+            List<DominioSKD.Matricula> l2 = new List<DominioSKD.Matricula>();
+            l2.Add(laMatricula2);
+
+
+            Assert.AreEqual(l, l2);
+
+        }
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Limpiamos todos los atributos usados
+        /// </summary>
+        /// <summary>
+        /// Limpiamos todos los atributos usados
+        /// </summary>
+        [TearDown]
+        public void limpiar()
+        {
+
+            l = null;
+        }
+        #endregion
+    }
+}
+
