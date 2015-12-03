cb410eac4536771c4a6aa7c262a7ebaad92caed7
nainzen <jorge_lgg@live.com>
2015-12-03 01:01:26 -0430

    Revert "Revert "Merge branch 'M10'""
    This reverts commit f29ef5fcdbffdee3903d2c2ec7b8fd8d7cf8deca.

--- GUI/Modulo10/M10_ListarAsistenciaEventos.aspx.cs
+++ GUI/Modulo10/M10_ListarAsistenciaEventos.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DominioSKD;
+using LogicaNegociosSKD.Modulo10;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +11,13 @@ namespace templateApp.GUI.Modulo10
 {
     public partial class M10_ListarAsistenciaEventos : System.Web.UI.Page
     {
+        List<Evento> eventos = new List<Evento>();
+        List<Competencia> competencias = new List<Competencia>();
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "10";
 
-            String success = Request.QueryString["eliminacionSuccess"];
-
+            String success = Request.QueryString["success"];
             if (success != null)
             {
                 if (success.Equals("1"))
@@ -31,6 +34,52 @@ namespace templateApp.GUI.Modulo10
                     alert.InnerHtml = "<div><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Asistencia modificada exitosamente</div>";
                 }
             }
+
+            #region Carga de tabla de Eventos y Competencias
+            if (!IsPostBack)
+            {
+                try
+                {
+                    eventos = LogicaAsistencia.ListarEventosAsistidos();
+                    foreach (Evento evento in eventos)
+                    {
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTR;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + evento.Id_evento.ToString() + M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + evento.Nombre.ToString() + M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + String.Format("{0:dd/MM/yyyy}", evento.Horario.FechaInicio) + M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + M10_RecursosInterfaz.Evento + " " + evento.TipoEvento.Nombre.ToString() + M10_RecursosInterfaz.CerrarTD;
+
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.BotonInfo + evento.Id_evento + M10_RecursosInterfaz.BotonCerrar;
+                        this.dataTable.Text += M10_RecursosInterfaz.BotonModificarEvento + evento.Id_evento + M10_RecursosInterfaz.BotonCerrar;
+                        this.dataTable.Text += M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.CerrarTR;
+                    }
+
+                    competencias = LogicaAsistencia.ListarCompetenciasAsistidas();
+                    foreach (Competencia competencia in competencias)
+                    {
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTR;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + competencia.Id_competencia.ToString() + M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + competencia.Nombre.ToString() + M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + String.Format("{0:dd/MM/yyyy}", competencia.FechaInicio) + M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD + M10_RecursosInterfaz.Competencia + M10_RecursosInterfaz.CerrarTD;
+
+                        this.dataTable.Text += M10_RecursosInterfaz.AbrirTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.BotonInfo + competencia.Id_competencia + M10_RecursosInterfaz.BotonCerrar;
+                        this.dataTable.Text += M10_RecursosInterfaz.BotonModificarCompetencia + competencia.Id_competencia + M10_RecursosInterfaz.BotonCerrar;
+                        this.dataTable.Text += M10_RecursosInterfaz.CerrarTD;
+                        this.dataTable.Text += M10_RecursosInterfaz.CerrarTR;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            #endregion
         }
+
+
     }
 }
