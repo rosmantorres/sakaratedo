bb1b67e332c3fe077f77504eba67413b7b0ff788
leojma <leojma@gmail.com>
2015-12-02 21:07:19 -0430

    Info producto top 5 40%

--- GUI/Modulo13/M13_Info_Producto.aspx.cs
+++ GUI/Modulo13/M13_Info_Producto.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LogicaNegociosSKD.Modulo13;
+using System.Data.SqlClient;
 //using System.Data;
 
 namespace templateApp.GUI.Modulo13
@@ -13,6 +15,85 @@ namespace templateApp.GUI.Modulo13
         protected void Page_Load(object sender, EventArgs e)
         {
             ((SKD)Page.Master).IdModulo = "13";
-        }
+
+            try
+            {
+                TableRow fila = new TableRow();
+                TableCell celda = new TableCell();
+                celda.Controls.Add(new LiteralControl("valor"));
+                fila.Cells.Add(celda);
+                example.Rows.Add(fila);
+                
+               SqlDataReader resultado;
+               resultado = LogicaNegociosSKD.Modulo13.LogicaInfo_Producto.L_Info_Producto(DropDownList1.SelectedItem.Value);
+               
+               /*
+               List<Persona> laLista;
+               LogicaAtletaCinta logCinta = new LogicaAtletaCinta();
+               laLista = logCinta.obtenerListaPersona();
+                */
+                while (resultado.Read())
+               {
+
+                //Response.Write("<tr>");
+                //Response.Write("<td>" + valor.Nombre + "</td>");
+                celda.Controls.Add(new LiteralControl(resultado[0].ToString()));
+                fila.Cells.Add(celda);
+                celda.Controls.Add(new LiteralControl(resultado[1].ToString()));
+                fila.Cells.Add(celda);
+                celda.Controls.Add(new LiteralControl(resultado[2].ToString()));
+                fila.Cells.Add(celda);
+                /*
+                    celda.Controls.Add(new LiteralControl(String.Format("{0:0,0.0000000}", valor.Estatura)));
+                fila.Cells.Add(celda);
+                celda.Controls.Add(new LiteralControl(String.Format("{0:0,0.0000000}",valor.Estatura)));
+                fila.Cells.Add(celda);
+                //celda.Controls.Add(new LiteralControl(valor.Nombre));
+                //fila.Cells.Add(celda);
+                //Response.Write("<td>" + valor.Apellido + "</td>");
+                //Response.Write("<td>" + valor.Edad + "</td>");
+                //Response.Write("<td>" + valor.Peso + "</td>");
+                //Response.Write("<td>" + valor.Estatura + "</td>");
+                    */
+
+                 example.Rows.Add(fila);
+               }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+
+            }
+
+
+
+
+        
+
+         protected void Button1_Click(object sender, EventArgs e) 
+         {
+             if (DropDownList1.SelectedValue == "-1")
+             {
+                 Response.Write("Porvafor Selecciona un Continente");
+
+             }
+             else
+             {   
+                  Response.Write(DropDownList1.SelectedItem.Text+"<br/>");
+                  Response.Write(DropDownList1.SelectedItem.Value + "<br/>");
+                  Response.Write(DropDownList1.SelectedIndex + "<br/>");
+             }
+
+             
+         }
+
+
     }
 }
+
+
+
+  
---
+++ LogicaNegociosSKD/Modulo13/LogicaInfo_Producto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominioSKD;
+using DatosSKD.Modulo13;
+using System.Data;
+using ExcepcionesSKD.Modulo15;
+using ExcepcionesSKD;
+using System.Data.SqlClient;
+
+namespace LogicaNegociosSKD.Modulo13
+{
+     public class LogicaInfo_Producto
+    {
+
+        #region
+        private List<Implemento> listaImplementos;
+        #endregion
+
+         #region constructor
+        public LogicaInfo_Producto()
+        {
+         
+        }
+        #endregion
+
+
+        #region Inventario
+
+        public static SqlDataReader L_Info_Producto(String dojo)
+        {
+            try
+            {
+                             
+                return (DatosSKD.Modulo13.BDimplementos.D_Info_producto(dojo));
+                   
+            }
+
+            catch (ExceptionSKDConexionBD ex)
+            {
+                throw ex;
+            }
+
+            catch (ErrorEnParametroDeProcedure ex)
+            {
+
+                throw ex;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return resultado;
+        }
+        
+        
+        #endregion
+
+    }
+}
