5c707d259f556578013137b1e09b19659c3b54e0
yunethm26 <yunethmolina2603@gmail.com>
2015-12-17 20:27:13 -0430

    Mas cambios

--- ScriptsBD/Stored_Procedure_M5.sql
+++ ScriptsBD/Stored_Procedure_M5.sql
@@ -1,5 +1,53 @@
 ---------------------------------------------------STORED PROCEDURES M5-------------------------------------
 
+--PROCEDURE AGREGAR CINTA
+CREATE PROCEDURE M5_AgregarCinta
+	@colorCinta			 	[varchar](100),
+	@rangoCinta			 	[varchar](100),
+	@clasificacionCinta		[varchar](100),
+	@significadoCinta	    [varchar](100),
+	@ordenCinta	        	[int],	
+	@idOrganizacion			[int],
+	@nombreOrganizacion  	[varchar](100)
+ 
+as
+ begin
+	declare @idCinta as int;
+
+	INSERT INTO CINTA(cin_color_nombre, cin_rango, cin_clasificacion, cin_significado, cin_orden) 
+				VALUES (@colorCinta, @rangoCinta, @clasificacionCinta, @significadoCinta, @ordenCinta);
+	
+	select @idCinta = max(cin_id) from cinta;
+	
+	INSERT INTO ORGANIZACION_CINTA (ORGANIZACION_org_id, CINTA_cin_id) 
+				VALUES (@idOrganizacion, @idCinta);
+		
+ end;
+ 
+ 
+--PROCEDURE BUSCAR ID DE CINTA
+CREATE procedure M5_BuscarIdCinta
+	@color_cinta [varchar](100)
+	
+as
+declare @id_cinta     [int]
+	begin
+		select @id_cinta = cin_id from cinta
+		where cin_color_nombre = @color_cinta
+	end;
+	
+--Para Cesar
+CREATE procedure M5_BuscarNombreCinta
+	@color_cinta [varchar](100)
+	
+as
+declare @nombre_cinta  [varchar](100)
+	begin
+		select @nombre_cinta  = cin_color_nombre from cinta
+		where cin_color_nombre = @color_cinta
+	end;
+
+
 --PROCEDURE CONSULTA LISTA DE CINTAS 
 CREATE procedure M5_ConsultarCintas
 as
@@ -11,27 +59,7 @@ as
 		from  CINTA cin, ORGANIZACION_CINTA orgcin, ORGANIZACION org
 		where orgcin.CINTA_cin_id = cin.cin_id  and orgcin.ORGANIZACION_org_id = org.org_id	
 	end;
-go
---COMO PROBAR EL PROCEDIMIENTO
---execute M5_ConsultarCintas;
 
-
---PROCEDURE CONSULTA LISTA DE CINTAS POR IDORGANIZACION
-CREATE procedure M5_ConsultarCintasXOrganizacionId
-	@id_organizacion [int]
-as
-	begin
-		select cin.cin_id as idCinta, cin.cin_color_nombre as colorCinta, cin.cin_rango as rangoCinta, 
-		cin.cin_clasificacion as ClasificacionCinta,
-		cin.cin_significado as significadoCinta, cin.cin_orden as ordenCinta,
-		org.org_id as idOrganizacion,org.org_nombre as nombreOrganizacion
-		from  CINTA cin, ORGANIZACION_CINTA orgcin, ORGANIZACION org
-		where orgcin.CINTA_cin_id = cin.cin_id  and orgcin.ORGANIZACION_org_id = org.org_id	and
-		org.org_id = @id_organizacion	
-	end;
-go
---COMO PROBAR EL PROCEDIMIENTO
---execute M5_ConsultarCintasXOrganizacionId "2";
 
 --PROCEDURE CONSULTA LISTA DE CINTAS POR ID, para el de "i"
 CREATE procedure M5_ConsultarCintasXId
@@ -46,10 +74,21 @@ as
 		where orgcin.CINTA_cin_id = cin.cin_id  and orgcin.ORGANIZACION_org_id = org.org_id	
 		and @id_cinta = cin.cin_id  
 	end;
-go
---COMO PROBAR EL PROCEDIMIENTO
---execute M5_ConsultarCintasXId "2";
-
+	
+--PROCEDURE CONSULTA LISTA DE CINTAS POR IDORGANIZACION
+CREATE procedure M5_ConsultarCintasXOrganizacionId
+	@id_organizacion [int]
+as
+	begin
+		select cin.cin_id as idCinta, cin.cin_color_nombre as colorCinta, cin.cin_rango as rangoCinta, 
+		cin.cin_clasificacion as ClasificacionCinta,
+		cin.cin_significado as significadoCinta, cin.cin_orden as ordenCinta,
+		org.org_id as idOrganizacion,org.org_nombre as nombreOrganizacion
+		from  CINTA cin, ORGANIZACION_CINTA orgcin, ORGANIZACION org
+		where orgcin.CINTA_cin_id = cin.cin_id  and orgcin.ORGANIZACION_org_id = org.org_id	and
+		org.org_id = @id_organizacion	
+	end;
+	
 
 --PROCEDURE ELIMINAR CINTA --- SOLO BORRO CINTA
 CREATE procedure M5_EliminarCinta
@@ -59,67 +98,7 @@ as
 	    delete cinta from cinta cin
 		where @id_cinta = cin.cin_id
 	end;
-go
-
---COMO PROBAR EL PROCEDIMIENTO execute M5_EliminarCinta "4";
-
---PROCEDURE BUSCAR ID DE CINTA
-CREATE procedure M5_BuscarIdCinta
-	@color_cinta [varchar](100)
 	
-as
-declare @id_cinta     [int]
-	begin
-		select @id_cinta = cin_id from cinta
-		where cin_color_nombre = @color_cinta
-	end;
-go
-
--- execute M5_BuscarIdCinta "Amarillo"
-
-
---Para Cesar
-CREATE procedure M5_BuscarNombreCinta
-	@color_cinta [varchar](100)
-	
-as
-declare @nombre_cinta  [varchar](100)
-	begin
-		select @nombre_cinta  = cin_color_nombre from cinta
-		where cin_color_nombre = @color_cinta
-	end;
-go
-
-
---PROCEDURE AGREGAR CINTA
-CREATE PROCEDURE M5_AgregarCinta
-	@colorCinta			 	[varchar](100),
-	@rangoCinta			 	[varchar](100),
-	@clasificacionCinta		[varchar](100),
-	@significadoCinta	    [varchar](100),
-	@ordenCinta	        	[int],	
-	@nombreOrganizacion  	[varchar](100)
- 
-as
- begin
-	declare @idOrganizacion as int;
-	declare @idCinta as int;
-
-	select @idOrganizacion = org_id from ORGANIZACION where org_nombre = @nombreOrganizacion;
-
-	INSERT INTO CINTA(cin_color_nombre, cin_rango, cin_clasificacion, cin_significado, cin_orden) 
-				VALUES (@colorCinta, @rangoCinta, @clasificacionCinta, @significadoCinta, @ordenCinta);
-	
-	select @idCinta = max(cin_id) from cinta;
-	
-	INSERT INTO ORGANIZACION_CINTA (ORGANIZACION_org_id, CINTA_cin_id) 
-				VALUES (@idOrganizacion, @idCinta);
-		
- end;
-go
-
---COMO PROBAR EL PROCEDIMIENTO execute M5_AgregarCinta 'Blanco', '1er Kyu', 'Nivel inferior', 'Principiante', '1', 'Clash Cobra-do';
-
 
 
 --PROCEDURE MODIFICAR CINTA--
@@ -130,20 +109,21 @@ CREATE PROCEDURE M5_ModificarCinta
 	@clasificacionCinta		[varchar](100),
 	@significadoCinta	    [varchar](100),
 	@ordenCinta	        	[int],
+	@idOrganizacion			[int],
 	@nombreOrganizacion		[varchar](100)
  
 as
  begin
-	declare @idOrganizacionNueva as int;
-	declare @idOrg as int;
+	declare @idOrg as int
 
-	select @idOrganizacionNueva = org_id from ORGANIZACION where org_nombre = @nombreOrganizacion;
+
+--	@idOrganizacionNueva = @idOrganizacion
 	
 	select @idOrg = orgcin.ORGANIZACION_org_id from  CINTA cin, ORGANIZACION_CINTA orgcin
 		where orgcin.CINTA_cin_id = cin.cin_id  and cin.cin_id = @idcinta
 	
 
-	if(@idOrganizacionNueva = @idOrg) --Es decir si no se cambia la org
+	if(@idOrganizacion = @idOrg) --Es decir si no se cambia la org
 	UPDATE CINTA
 		SET 
 			cin_color_nombre  = @colorCinta,
@@ -153,7 +133,7 @@ as
 			cin_orden		  = @ordenCinta	
 			WHERE
 			cin_id = @idCinta;	
-	else
+	 else
 		delete ORGANIZACION_CINTA from ORGANIZACION_CINTA orgcin 
 					where orgcin.ORGANIZACION_org_id = @idOrg and orgcin.CINTA_cin_id = @idcinta;
 		
@@ -168,10 +148,7 @@ as
 			cin_id = @idcinta;	
 		
 		
-		INSERT INTO ORGANIZACION_CINTA(ORGANIZACION_org_id,CINTA_cin_id) VALUES(@idOrganizacionNueva,@idcinta);
+		INSERT INTO ORGANIZACION_CINTA(ORGANIZACION_org_id,CINTA_cin_id) VALUES(@idOrganizacion,@idcinta);
 	
 	
  end;
-go
-
---COMO PROBAR EL PROCEDIMIENTO execute M5_ModificarCinta 'Clash Cobra-do','3','Blanco', '1er Kyu', 'Nivel inferior', 'Principiante', '1';
