0bffbd222b0b7cf662734906e153e4725ccf2604
Manuel28G <man281192@gmail.com>
2015-12-30 07:18:56 -0430

    M1_ContratoMaster

--- Interfaz_Contratos/IContratoMasterPage.cs
+++ Interfaz_Contratos/IContratoMasterPage.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Interfaz_Contratos
 {
     public interface IContratoMasterPage
     {
+        String MenuSuperiorList { get; set; }
+
+        String DES { get; set; }
+
+        String MenuLateralList { get; set; }
+
+        String IdModulo { get; set; }
+
+        String RolEnUso { get; set; }
+
+        String[] RolesUsuario { get; set; }
+
+        String RolesList { get; set; }
+
+        String SessionImagen { get; set; }
+
+        String SessionUsuarioNombre { get; }
+
+        String SessionNombreCompleto { get; }
     }
 }
