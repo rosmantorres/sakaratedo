9f4b0652df7cb534e12a6a38ebbce56735afce44
Pedro Villarreal <pedrovillarreal@gmail.com>
2016-01-03 12:08:47 -0430

    M7_IDao
    se agregó las interfaces de todas las clases necesarias

---
+++ DatosSKD/InterfazDAO/Modulo7/IDaoEvento.cs
@@ -0,0 +1,14 @@
+﻿using DominioSKD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosSKD.InterfazDAO.Modulo7
+{
+    public interface IDaoEvento : IDao<Entidad, bool, Entidad>
+    {
+
+    }
+}
