5b84532a59a9f354528653acbe82ff81d204160b
Pedro Villarreal <pedrovillarreal@gmail.com>
2016-01-04 15:50:28 -0430

    M7_DAOCinta&PruebaDaoCinta
    Se terminó la implementación de los metodos de DAO Cinta y se agregó
    algunas pruebas unitarias de DAO cinta (falta completar con fabrica de
    entidades)

--- DatosSKD/DAO/Modulo7/DaoCinta.cs
+++ DatosSKD/DAO/Modulo7/DaoCinta.cs
@@ -15,13 +15,22 @@ namespace DatosSKD.DAO.Modulo7
 {
     public class DaoCinta : DAOGeneral, IDaoCinta
     {
-        #region IDao
+        #region IDaoCinta
 
+        /// <summary>
+        /// No tiene implementación
+        /// </summary>
+        /// <param name="parametro"></param>
+        /// <returns></returns>
         public bool Agregar(Entidad parametro)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// No tiene implementación
+        /// </summary>
+        /// <returns></returns>
         public List<Entidad> ConsultarTodos()
         {
             throw new NotImplementedException();
@@ -40,19 +49,16 @@ namespace DatosSKD.DAO.Modulo7
             BDConexion conexion;
             List<Parametro> parametros;
             Parametro parametroQuery = new Parametro();
-
-            FabricaEntidades fabrica = new FabricaEntidades();
-            Cinta cinta = (Cinta)fabrica.ObtenerCinta();          
+            FabricaEntidades fabricaEntidades = new FabricaEntidades();
             Cinta idCinta = (Cinta)parametro;
+            Cinta cinta = (Cinta)fabricaEntidades.ObtenerCinta();
 
             try
             {
                 if (idCinta.Id > 0)
                 {
                     conexion = new BDConexion();
-                    parametros = new List<Parametro>();
-                    cinta = new Cinta();
-
+                    parametros = new List<Parametro>();                    
                     parametroQuery = new Parametro(RecursosDAOModulo7.ParamIdCinta, SqlDbType.Int, idCinta.Id.ToString(), false);
                     parametros.Add(parametroQuery);
 
@@ -110,9 +116,83 @@ namespace DatosSKD.DAO.Modulo7
             return cinta;
         }
 
+        /// <summary>
+        /// Método para consultar la fecha de obtencion de una cinta perteneciente a un atleta
+        /// </summary>
+        /// <param name="persona">Objeto tipo Entidad que posee el id de la persona</param>
+        /// <param name="cinta">Objeto tipo Entidad que posee el id de la cinta</param>
+        /// <returns>Retorna fecha de obtencion de una cinta</returns>
         public DateTime FechaCinta(Entidad persona, Entidad cinta)
         {
-            throw new NotImplementedException();
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                RecursosDAOModulo7.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            BDConexion laConexion;
+            List<Parametro> parametros;
+            Parametro parametroPersona = new Parametro();
+            Parametro parametroCinta = new Parametro();
+            DateTime fechaCinta = new DateTime();
+            Persona idPersona = (Persona)persona;
+            Cinta idCinta = (Cinta)cinta;
+
+            try
+            {
+                if (idPersona.ID > 0 && idCinta.Id > 0)
+                {
+                    laConexion = new BDConexion();
+                    parametros = new List<Parametro>();
+                    parametroPersona = new Parametro(RecursosDAOModulo7.ParamIdPersona, SqlDbType.Int, idPersona.ID.ToString(), false);
+                    parametroCinta = new Parametro(RecursosDAOModulo7.ParamIdCinta, SqlDbType.Int, idCinta.Id.ToString(), false);
+                    parametros.Add(parametroPersona);
+                    parametros.Add(parametroCinta);
+
+                    DataTable dt = laConexion.EjecutarStoredProcedureTuplas(
+                                   RecursosDAOModulo7.ConsultarFechaCinta, parametros);
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        fechaCinta = DateTime.Parse(row[RecursosDAOModulo7.AliasCintaFecha].ToString());
+                    }
+                }
+                else
+                {
+                    throw new NumeroEnteroInvalidoException(RecursosDAOModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosDAOModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (NumeroEnteroInvalidoException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new NumeroEnteroInvalidoException(RecursosDAOModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosDAOModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+            }
+            catch (FormatException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new NumeroEnteroInvalidoException(RecursosDAOModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosDAOModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+            }
+            catch (ExceptionSKDConexionBD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExceptionSKD("No se pudo completar la operacion", ex);
+            }
+
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                RecursosDAOModulo7.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            return fechaCinta;
         }
 
         /// <summary>
@@ -129,7 +209,7 @@ namespace DatosSKD.DAO.Modulo7
             List<Parametro> parametros;
             Parametro parametroQuery = new Parametro();
 
-            FabricaEntidades fabrica = new FabricaEntidades();            
+            FabricaEntidades fabricaEntidades = new FabricaEntidades();            
             List<Entidad> listaDeCintas = new List<Entidad>();
             Persona idPersona = (Persona)persona;
 
@@ -147,7 +227,7 @@ namespace DatosSKD.DAO.Modulo7
 
                     foreach (DataRow row in dt.Rows)
                     {
-                        Cinta cinta = (Cinta)fabrica.ObtenerCinta();
+                        Cinta cinta = (Cinta)fabricaEntidades.ObtenerCinta();
                         cinta.Id_cinta = int.Parse(row[RecursosDAOModulo7.AliasIdCinta].ToString());
                         cinta.Color_nombre = row[RecursosDAOModulo7.AliasCintaNombre].ToString();
                         cinta.Rango = row[RecursosDAOModulo7.AliasCintaRango].ToString();
@@ -163,8 +243,6 @@ namespace DatosSKD.DAO.Modulo7
                     throw new NumeroEnteroInvalidoException(RecursosDAOModulo7.Codigo_Numero_Parametro_Invalido,
                                 RecursosDAOModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
                 }
-
-
 
             }
             catch (SqlException ex)
@@ -203,14 +281,93 @@ namespace DatosSKD.DAO.Modulo7
             return listaDeCintas;
         }
 
+        /// <summary>
+        /// No tiene implementación
+        /// </summary>
+        /// <param name="parametro"></param>
+        /// <returns></returns>
         public bool Modificar(Entidad parametro)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Método para consultar la ultima cinta de un atleta
+        /// </summary>
+        /// <param name="persona">Objeto tipo Entidad que posee el id de la persona a consultar</param>
+        /// <returns>Retorna objeto de tipo Entidad con la informacion de la cinta</returns>
         public Entidad UltimaCinta(Entidad persona)
         {
-            throw new NotImplementedException();
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+               RecursosDAOModulo7.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            BDConexion laConexion;
+            List<Parametro> parametros;
+            Parametro elParametro = new Parametro();
+            FabricaEntidades fabricaEntidades = new FabricaEntidades();
+            Persona idPersona = (Persona)persona;
+            Cinta cinta = (Cinta)fabricaEntidades.ObtenerCinta();
+
+            try
+            {
+                if (idPersona.ID > 0)
+                {
+                    laConexion = new BDConexion();
+                    parametros = new List<Parametro>();          
+                    elParametro = new Parametro(RecursosDAOModulo7.ParamIdPersona, SqlDbType.Int, idPersona.ID.ToString(), false);
+                    parametros.Add(elParametro);
+
+                    DataTable dt = laConexion.EjecutarStoredProcedureTuplas(
+                                   RecursosDAOModulo7.ConsultarUltimaCinta, parametros);
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        cinta.Id_cinta = int.Parse(row[RecursosDAOModulo7.AliasIdCinta].ToString());
+                        cinta.Color_nombre = row[RecursosDAOModulo7.AliasCintaNombre].ToString();
+                        cinta.Rango = row[RecursosDAOModulo7.AliasCintaRango].ToString();
+                        cinta.Clasificacion = row[RecursosDAOModulo7.AliasCintaClasificacion].ToString();
+                        cinta.Significado = row[RecursosDAOModulo7.AliasCintaSignificado].ToString();
+                        cinta.Orden = int.Parse(row[RecursosDAOModulo7.AliasCintaOrden].ToString());
+                    }
+                }
+                else
+                {
+                    throw new NumeroEnteroInvalidoException(RecursosDAOModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosDAOModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (NumeroEnteroInvalidoException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new NumeroEnteroInvalidoException(RecursosDAOModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosDAOModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+            }
+            catch (FormatException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new NumeroEnteroInvalidoException(RecursosDAOModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosDAOModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+            }
+            catch (ExceptionSKDConexionBD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExceptionSKD("No se pudo completar la operacion", ex);
+            }
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                RecursosDAOModulo7.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            return cinta;
         }
 
         #endregion
--- PruebasUnitariasSKD/Modulo7/PruebasDAO/M7_PruebasDAOCinta.cs
+++ PruebasUnitariasSKD/Modulo7/PruebasDAO/M7_PruebasDAOCinta.cs
@@ -3,10 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using DatosSKD.Fabrica;
+using DatosSKD.DAO.Modulo7;
+using DominioSKD.Fabrica;
+using DominioSKD;
 
 namespace PruebasUnitariasSKD.Modulo7.PruebasDAO
 {
+    /// <summary>
+    /// Clase que contiene las pruebas unitarias para DaoCinta
+    /// </summary>
+    [TestFixture]
     public class M7_PruebasDAOCinta
     {
+        #region Atributos
+        Persona idPersona;
+        FabricaEntidades fabricaEntidades;
+        FabricaDAOSqlServer fabricaSql;
+        #endregion
+
+        #region SetUp & TearDown
+        [SetUp]
+        public void Init()
+        {
+            fabricaSql = new FabricaDAOSqlServer();
+            fabricaEntidades = new FabricaEntidades();
+            idPersona = new Persona();//esto se sustituye con fabrica de entidad
+            idPersona.ID = 6;
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            idPersona = null;
+            fabricaEntidades = null;
+            fabricaSql = null;
+        }
+        #endregion
+
+        #region Test
+        /// <summary>
+        /// Método de prueba para ConSultarXId en DAO
+        /// </summary>
+        [Test]
+        public void PruebaDetallarCintaXIdDAO()
+        {
+            //DaoCinta basedeDatosCinta = fabricaSql.nuevoDAOCinta();
+            DaoCinta baseDeDatosCinta = new DaoCinta();//esto se sustituye con la fabrica
+            Cinta idCinta = (Cinta)fabricaEntidades.ObtenerCinta();
+            idCinta.Id = 1;
+            Cinta cinta = (Cinta)baseDeDatosCinta.ConsultarXId(idCinta);
+            Assert.AreEqual("Blanco", cinta.Color_nombre);
+        }
+
+        /// <summary>
+        /// Método de prueba para ListarCintasObtenidas en DAO
+        /// </summary>
+        [Test]
+        public void PruebaListarCintasObtenidasDAO()
+        {
+            //DaoCinta basedeDatosCinta = fabricaSql.nuevoDAOCinta();
+            DaoCinta baseDeDatosCinta = new DaoCinta();//esto se sustituye con la fabrica
+            List<Entidad> listaCinta = baseDeDatosCinta.ListarCintasObtenidas(idPersona);
+            Assert.GreaterOrEqual(listaCinta.Count, 1);
+        }
+
+        /// <summary>
+        /// Método de prueba para UltimaCinta en DAO
+        /// </summary>
+        [Test]
+        public void PruebaUltimaCinta()
+        {
+            //DaoCinta basedeDatosCinta = fabricaSql.nuevoDAOCinta();
+            DaoCinta baseDeDatosCinta = new DaoCinta();//esto se sustituye con la fabrica
+            Cinta cinta = (Cinta)baseDeDatosCinta.UltimaCinta(idPersona);
+            Assert.AreEqual("Amarillo", cinta.Color_nombre);
+        }
+
+        /// <summary>
+        /// Método de prueba para ObtenerCinta en DAO
+        /// </summary>
+        [Test]
+        public void PruebaFechaObtencionCinta()
+        {
+            //DaoCinta basedeDatosCinta = fabricaSql.nuevoDAOCinta();
+            DaoCinta baseDeDatosCinta = new DaoCinta();//esto se sustituye con la fabrica
+            Cinta idCinta = (Cinta)fabricaEntidades.ObtenerCinta();
+            idCinta.Id = 2;
+            Assert.AreEqual("08/21/2015", baseDeDatosCinta.FechaCinta(idPersona, idCinta).ToString("MM/dd/yyyy"));
+        }
+        #endregion
     }
 }
