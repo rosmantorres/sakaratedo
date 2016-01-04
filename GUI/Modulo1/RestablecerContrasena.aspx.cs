a2b26bbed3bc12527b8b87bc75a024022242dc97
Manuel28G <man281192@gmail.com>
2016-01-04 04:34:17 -0430

    M1_PatronMVP Restablecer

--- GUI/Modulo1/RestablecerContrasena.aspx.cs
+++ GUI/Modulo1/RestablecerContrasena.aspx.cs
@@ -7,106 +7,57 @@ using System.Web.UI.WebControls;
 using LogicaNegociosSKD.Modulo1;
 using LogicaNegociosSKD.Modulo2;
 using templateApp.GUI.Master;
+using Interfaz_Presentadores.Modulo1;
+using Interfaz_Contratos.Modulo1;
 
 namespace templateApp.GUI.Modulo1
 {
-    public partial class RestablecerContraseña : System.Web.UI.Page
+    public partial class RestablecerContraseña : System.Web.UI.Page , IContratoM1Restablecer
     {
         
         string IdUser = "";
         string value;
+        private PresentadorM1Restablecer presentador;
         AlgoritmoDeEncriptacion cripto = new AlgoritmoDeEncriptacion();
+
+        String IContratoM1Restablecer.ClaveEtq
+        {
+            get { return password3.Value; }
+        }
+        String IContratoM1Restablecer.ClaveConfirmacionEtq
+        {
+            get { return password4.Value; }
+        }
+        String IContratoM1Restablecer.InfoRestablecerEtqText
+        {
+            get { return infoRestablecer.InnerText; }
+            set { infoRestablecer.InnerText = value; }
+        }
+        String IContratoM1Restablecer.WarningCaracterEtqText
+        {
+            get { return warningCaracteres.InnerText; }
+            set { warningCaracteres.InnerText = value; }
+        }
+        Boolean IContratoM1Restablecer.InfoRestablecerEtq
+        {
+            get { return infoRestablecer.Visible; }
+            set { infoRestablecer.Visible = value; }
+        }
+        Boolean IContratoM1Restablecer.WarningCaracterEtq
+        {
+            get { return warningCaracteres.Visible; }
+            set { warningCaracteres.Visible = value; }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            try
-            {
-
-                if (Request.QueryString[RecursosInterfazModulo1.tipoErrMalicioso] != null
-        && Request.QueryString[RecursosInterfazModulo1.tipoErrMalicioso].ToString() == "input_malicioso")
-                {
-                    warningCaracteres.Visible = true;
-                    warningCaracteres.InnerText = RecursosInterfazModulo1.logCadenaMaliciosa;
-                    infoRestablecer.Visible = false;
-
-                }
-                else
-                {
-                    infoRestablecer.Visible = true;
-                    warningCaracteres.Visible = false;
-                    warningCaracteres.InnerText = "";
-                }
-                DateTime fechaActual = DateTime.Now;
-                string fechaString = Request.QueryString[RecursosLogicaModulo1.variableFecha].ToString();
-                fechaString = cripto.DesencriptarCadenaDeCaracteres(fechaString,
-                    RecursosLogicaModulo2.claveDES);
-                DateTime fecha = Convert.ToDateTime(fechaString);
-                if ((fecha.Date.Year != fechaActual.Date.Year) ||
-                    (fecha.Date.Month != fechaActual.Date.Month) ||
-                    (fecha.Date.Day != fechaActual.Date.Day))
-                {
-                     value=cripto.EncriptarCadenaDeCaracteres
-                        (RecursosInterfazModulo1.parametroURLRestablecerCaducado, RecursosLogicaModulo2.claveDES);
-
-                    Response.Redirect(RecursosInterfazModulo1.direccionM1_Index + "?"
-                       + RecursosInterfazModulo1.tipoInfo + "=" +value);
-                }
-
-
-                string idUsuario = Request.QueryString[RecursosLogicaModulo1.variableRestablecer].ToString();
-                
-                idUsuario = cripto.DesencriptarCadenaDeCaracteres(idUsuario, RecursosLogicaModulo2.claveDES);
-                IdUser = idUsuario;
-            }
-            catch (NullReferenceException ex)
-            {
-
-            }
+            presentador = new PresentadorM1Restablecer(this);
+            presentador.Inicio();
+            
         }
         public void redireccionarInicio(object sender, EventArgs e)
         {
-            try
-            {
-                string pass1 = password3.Value;
-                string pass2 = password4.Value;
-                logicaRestablecer Restablecer = new logicaRestablecer();
-                if (pass1 != "" && pass1 == pass2
-                    && pass1.Length > 7 && IdUser != ""
-                    && Restablecer.ValidarCaracteres(pass1))
-                {
-
-                    if (Restablecer.restablecerContrasena(IdUser, pass1))
-                    {
-                        if (Session[RecursosInterfazMaster.sessionUsuarioID] == null)
-                        {
-                            value = cripto.EncriptarCadenaDeCaracteres
-                               (RecursosInterfazModulo1.parametroURLReestablecerExito, RecursosLogicaModulo2.claveDES);
-                            Response.Redirect(RecursosInterfazModulo1.direccionM1_Index + "?"
-                                + RecursosInterfazModulo1.tipoSucess + "=" + value);
-                        }
-                        else
-                            Response.Redirect(RecursosInterfazMaster.direccionMaster_Inicio);
-                    }
-
-                }
-                else if (!Restablecer.ValidarCaracteres(pass1))
-                {
-                    warningCaracteres.Visible = true;
-                    warningCaracteres.InnerText = RecursosInterfazModulo1.logCaracterInvalidos;
-                    infoRestablecer.Visible = false;
-                }
-                else
-                {
-                    infoRestablecer.Visible = true;
-                    warningCaracteres.Visible = false;
-                    warningCaracteres.InnerText = "";
-
-                }
-               
-            }
-            catch (Exception ex)
-            {
-               //imprimirMensajePorPantallaCnERR
-            }
+            presentador.RedireccionarInicio();
         }
       
     }
---
+++ Interfaz_Presentadores/Modulo1/PresentadorM1Restablecer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+using Interfaz_Contratos.Modulo1;
+using Interfaz_Presentadores.Master;
+using LogicaNegociosSKD.Modulo2;
+using LogicaNegociosSKD.Modulo1;
+
+namespace Interfaz_Presentadores.Modulo1
+{
+    public class PresentadorM1Restablecer
+    {
+        private IContratoM1Restablecer _iMaster;
+        private AlgoritmoDeEncriptacion cripto = new AlgoritmoDeEncriptacion();
+        string IdUser = "";
+        string value;
+
+        public PresentadorM1Restablecer(IContratoM1Restablecer iMaster)
+        {
+            _iMaster=iMaster;
+        }
+
+        public void Inicio()
+        {
+            try
+            {
+
+                if (HttpContext.Current.Request.QueryString[RecursosInterfazPresentadorM1.tipoErrMalicioso] != null
+        && HttpContext.Current.Request.QueryString[RecursosInterfazPresentadorM1.tipoErrMalicioso].ToString() == "input_malicioso")
+                {
+                    _iMaster.WarningCaracterEtq = true;
+                    _iMaster.WarningCaracterEtqText = RecursosInterfazPresentadorM1.logCadenaMaliciosa;
+                    _iMaster.InfoRestablecerEtq = false;
+
+                }
+                else
+                {
+                    _iMaster.InfoRestablecerEtq = true;
+                    _iMaster.WarningCaracterEtq = false;
+                    _iMaster.WarningCaracterEtqText = "";
+                }
+                DateTime fechaActual = DateTime.Now;
+                string fechaString = HttpContext.Current.Request.QueryString[RecursosLogicaModulo1.variableFecha].ToString();
+                fechaString = cripto.DesencriptarCadenaDeCaracteres(fechaString,
+                    RecursosLogicaModulo2.claveDES);
+                DateTime fecha = Convert.ToDateTime(fechaString);
+                if ((fecha.Date.Year != fechaActual.Date.Year) ||
+                    (fecha.Date.Month != fechaActual.Date.Month) ||
+                    (fecha.Date.Day != fechaActual.Date.Day))
+                {
+                    value = cripto.EncriptarCadenaDeCaracteres
+                       (RecursosInterfazPresentadorM1.parametroURLRestablecerCaducado, RecursosLogicaModulo2.claveDES);
+
+                    HttpContext.Current.Response.Redirect(RecursosInterfazPresentadorM1.direccionM1_Index + "?"
+                       + RecursosInterfazPresentadorM1.tipoInfo + "=" + value);
+                }
+
+
+                string idUsuario = HttpContext.Current.Request.QueryString[RecursosLogicaModulo1.variableRestablecer].ToString();
+
+                idUsuario = cripto.DesencriptarCadenaDeCaracteres(idUsuario, RecursosLogicaModulo2.claveDES);
+                IdUser = idUsuario;
+            }
+            catch (NullReferenceException ex)
+            {
+
+            }
+        }
+
+        public void RedireccionarInicio()
+        {
+            try
+            {
+                string pass1 = _iMaster.ClaveEtq;
+                string pass2 = _iMaster.ClaveConfirmacionEtq;
+                logicaRestablecer Restablecer = new logicaRestablecer();
+                if (pass1 != "" && pass1 == pass2
+                    && pass1.Length > 7 && IdUser != ""
+                    && Restablecer.ValidarCaracteres(pass1))
+                {
+
+                    if (Restablecer.restablecerContrasena(IdUser, pass1))
+                    {
+                        if (HttpContext.Current.Session[RecursosInterfazMaster.sessionUsuarioID] == null)
+                        {
+                            value = cripto.EncriptarCadenaDeCaracteres
+                               (RecursosInterfazPresentadorM1.parametroURLReestablecerExito, RecursosLogicaModulo2.claveDES);
+                            HttpContext.Current.Response.Redirect(RecursosInterfazPresentadorM1.direccionM1_Index + "?"
+                                + RecursosInterfazPresentadorM1.tipoSucess + "=" + value);
+                        }
+                        else
+                            HttpContext.Current.Response.Redirect(RecursosInterfazMaster.direccionMaster_Inicio);
+                    }
+
+                }
+                else if (!Restablecer.ValidarCaracteres(pass1))
+                {
+                    _iMaster.WarningCaracterEtq= true;
+                    _iMaster.WarningCaracterEtqText = RecursosInterfazPresentadorM1.logCaracterInvalidos;
+                    _iMaster.InfoRestablecerEtq = false;
+                }
+                else
+                {
+                    _iMaster.InfoRestablecerEtq= true;
+                    _iMaster.WarningCaracterEtq = false;
+                    _iMaster.WarningCaracterEtqText = "";
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //imprimirMensajePorPantallaCnERR
+            }
+        }
+    }
+}
