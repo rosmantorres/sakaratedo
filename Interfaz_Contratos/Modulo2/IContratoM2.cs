bd7c6ef17dd6a2dc333c5a1def1b1bca3883bfbb
Manuel28G <man281192@gmail.com>
2016-01-04 21:45:54 -0430

    M2_Contrato

--- Interfaz_Contratos/Modulo2/IContratoM2.cs
+++ Interfaz_Contratos/Modulo2/IContratoM2.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Interfaz_Contratos.Modulo2
 {
-    class IContratoM2
+    public interface IContratoM2
     {
+        String ImagenEtqSRC { get; set; }
+
+        String RolSelectEqt { get; }
+
+        String NombreApellidoEtq { set; }
+
+        String NombreUsuaurioEtq { set; }
+
+        String RolesUsuario { get; set; }
     }
 }
