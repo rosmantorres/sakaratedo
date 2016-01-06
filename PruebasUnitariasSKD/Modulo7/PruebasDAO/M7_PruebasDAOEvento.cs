bf797bab116cb1c78cd6453d4c1deea66e2e312d
adrianabetta <adri1191@hotmail.com>
2016-01-06 00:01:07 -0430

    M7_PruebasUnitariasDaoEvento(PagosAEventos)
    Se implementaron todas las pruebas unitarias referente a los pagos de
    eventos y pagos de competencias

--- PruebasUnitariasSKD/Modulo7/PruebasDAO/M7_PruebasDAOEvento.cs
+++ PruebasUnitariasSKD/Modulo7/PruebasDAO/M7_PruebasDAOEvento.cs
@@ -83,7 +83,72 @@ namespace PruebasUnitariasSKD.Modulo7.PruebasDAO
             Evento evento = (Evento)baseDeDatosEvento.ConsultarXId(idEvento);
         }
 
-      
+        /// <summary>
+        /// Método para probar que la lista obtenida tiene cero o mas eventos pagos
+        /// </summary>
+        [Test]
+        public void PruebaListarEventosPagos()
+        {
+            DaoEvento baseDeDatosEvento = new DaoEvento();//esto se sustituye con la fabrica
+            List<Entidad> listaEvento = baseDeDatosEvento.ListarEventosPagos(idPersona);
+            Assert.GreaterOrEqual(listaEvento.Count, 0);
+        }
+        /// <summary>
+        /// Método para probar que la lista obtenida no sea nula
+        /// </summary>
+        [Test]
+        public void PruebaListarEventosPagosNoNulo()
+        {
+            DaoEvento baseDeDatosEvento = new DaoEvento();//esto se sustituye con la fabrica
+            List<Entidad> listaEvento = baseDeDatosEvento.ListarEventosPagos(idPersona);
+            Assert.NotNull(listaEvento);
+        }
 
+        /// <summary>
+        /// Método para probar la exception de número entero invalido de listar eventos pagos
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(NumeroEnteroInvalidoException))]
+        public void ListarEventoPagoNumeroEnteroException()
+        {
+            DaoEvento baseDeDatosEvento = new DaoEvento();//esto se sustituye con la fabrica
+            idPersona.ID = -1;
+            List<Entidad> listaEvento = baseDeDatosEvento.ListarEventosPagos(idPersona);
+           
+        }
+        /// <summary>
+        /// Método para probar que la lista obtenida tiene cero o mas competencias pagadas por atleta
+        /// </summary>
+        [Test]
+        public void PruebaListarCompetenciasPagas()
+        {
+            DaoEvento baseDeDatosCompetencia = new DaoEvento();// se sustituye con la fabrica
+            List<Entidad> listaCompetencia = baseDeDatosCompetencia.ListarCompetenciasPaga(idPersona);
+            Assert.GreaterOrEqual(listaCompetencia.Count, 0);
+        }
+
+        /// <summary>
+        /// Método para probar que la lista obtenida no sea nula
+        /// </summary>
+        [Test]
+        public void PruebaListarCompetenciasPagasNoNula()
+        {
+            DaoEvento baseDeDatosCompetencia = new DaoEvento();// se sustituye con la fabrica
+            List<Entidad> listaCompetencia = baseDeDatosCompetencia.ListarCompetenciasPaga(idPersona);
+            Assert.NotNull(listaCompetencia);
+        }
+
+        /// <summary>
+        /// Método para probar la exception de número entero invalido de listar competencias pagadas
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(NumeroEnteroInvalidoException))]
+        public void ListarCompetenciaPagaNumeroEnteroException()
+        {
+            DaoEvento baseDeDatosCompetencia = new DaoEvento();// se sustituye con la fabrica
+            idPersona.ID = -1;
+            List<Entidad> listaCompetencia = baseDeDatosCompetencia.ListarCompetenciasPaga(idPersona);
+        }
+         
     }
 }
