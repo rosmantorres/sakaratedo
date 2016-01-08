28de427cf50f5bda085fc790f73f6e56875a442f
Minervavr <minerva_veru@hotmail.com>
2016-01-07 21:48:33 -0430

    M16_PruebasDetallarDaoyComando
    Se sube la estrustura de las pruebas preparadas para probar los detallar
    en la capa de datos (DAO) y en la capa de Logica (COMANDOS).

---
+++ PruebasUnitariasSKD/Modulo16/PruebaComandoDetallarImplemento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LogicaNegociosSKD.Comandos.Modulo16;
+using DominioSKD;
+using DominioSKD.Entidades.Modulo15;
+using DominioSKD.Entidades.Modulo6;
+using DominioSKD.Entidades.Modulo1;
+using LogicaNegociosSKD.Fabrica;
+using LogicaNegociosSKD.Comandos;
+using LogicaNegociosSKD;
+
+namespace PruebasUnitariasSKD.Modulo16
+{
+    class PruebaComandoDetallarImplemento
+    {
+    }
+}
---
+++ PruebasUnitariasSKD/Modulo16/PruebaDaoDetallarImplemento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DominioSKD;
+using DominioSKD.Entidades.Modulo1;
+using DominioSKD.Entidades.Modulo6;
+using DominioSKD.Entidades.Modulo15;
+using DatosSKD.DAO.Modulo16;
+using DatosSKD;
+using DatosSKD.DAO;
+using DatosSKD.Fabrica;
+using DatosSKD.InterfazDAO.Modulo16;
+
+namespace PruebasUnitariasSKD.Modulo16
+{
+    class PruebaDaoDetallarImplemento
+    {
+    }
+}
