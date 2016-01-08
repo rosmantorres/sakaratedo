1a7c5cc220eb432a188068e985f4dc0406860128
Minervavr <minerva_veru@hotmail.com>
2016-01-07 21:39:57 -0430

    M16_PruebasDaoConsultarImplemento,Matricula,Evento
    Se implementan las pruebas para la capa de datos correspondientes a los
    DaoEvento, DaoMatricula, DaoImplemento.

--- PruebasUnitariasSKD/Modulo16/PruebaDaoConsultarEventos.cs
+++ PruebasUnitariasSKD/Modulo16/PruebaDaoConsultarEventos.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using DominioSKD;
+using DominioSKD.Entidades.Modulo1;
+using DominioSKD.Entidades.Modulo6;
+using DominioSKD.Entidades.Modulo15;
+using DatosSKD.DAO.Modulo16;
+using DatosSKD;
+using DatosSKD.DAO;
+using DatosSKD.Fabrica;
+using DatosSKD.InterfazDAO.Modulo16;
+
+
 
 namespace PruebasUnitariasSKD.Modulo16
 {
     class PruebaDaoConsultarEventos
     {
+        /*
+
+        #region Atributos
+        //Atributos pertinentes a usar
+        private DaoEvento pruebaDao;
+        private IdaoEvento daoEvento;
+        #endregion
+
+
+        /// <summary>
+        /// Prepara todos los atributos que utilizaremos para probar
+        /// </summary>
+        [SetUp]
+        public void Iniciar()
+        {
+          
+            //Obtengo el comando
+            this.pruebaDao = (DaoEvento)FabricaDAOSqlServer.ObtenerDaoEventos();
+
+
+        }
+
+
+
+        /// <summary>
+        /// prueba para consultar eventos
+        /// </summary>
+        [Test]
+        public void pruebaConsultarEventos()
+        {
+            daoEvento = FabricaDAOSqlServer.ObtenerDaoEventos();
+            Assert.IsNotNull(daoEvento.ListarEvento());
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// Elimina todos los atributos utilizados al probar
+        /// </summary>
+        [TearDown]
+        public void Limpiar()
+        {
+            this.pruebaDao = null;
+            this.daoEvento = null;
+        }
+
+  */
     }
+       
 }
--- PruebasUnitariasSKD/Modulo16/PruebaDaoConsultarMatriculas.cs
+++ PruebasUnitariasSKD/Modulo16/PruebaDaoConsultarMatriculas.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using DominioSKD;
+using DominioSKD.Entidades.Modulo1;
+using DominioSKD.Entidades.Modulo6;
+using DominioSKD.Entidades.Modulo15;
+using DatosSKD.DAO.Modulo16;
+using DatosSKD;
+using DatosSKD.DAO;
+using DatosSKD.Fabrica;
+using DatosSKD.InterfazDAO.Modulo16;
 
 namespace PruebasUnitariasSKD.Modulo16
 {
     class PruebaDaoConsultarMatriculas
     {
+        /*
+
+        #region Atributos
+        //Atributos pertinentes a usar
+        private DaoMensualidad pruebaDao;
+        private IdaoMensualidad daoMatricula;
+        #endregion
+
+
+        /// <summary>
+        /// Prepara todos los atributos que utilizaremos para probar
+        /// </summary>
+        [SetUp]
+        public void Iniciar()
+        {
+
+            //Obtengo el comando
+            this.pruebaDao = (DaoMensualidad)FabricaDAOSqlServer.ObtenerDaoMensualidades();
+
+
+        }
+
+
+
+        /// <summary>
+        /// prueba para consultar matriculas
+        /// </summary>
+        [Test]
+        public void pruebaConsultarImplementos()
+        {
+            daoMatricula = FabricaDAOSqlServer.ObtenerDaoMensualidades();
+            Assert.IsNotNull(daoMatricula.ListarMensualidad());
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// Elimina todos los atributos utilizados al probar
+        /// </summary>
+        [TearDown]
+        public void Limpiar()
+        {
+            this.pruebaDao = null;
+            this.daoMatricula = null;
+        }
+         */ 
     }
 }
