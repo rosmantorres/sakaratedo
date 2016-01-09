315e1facbaddfa90d8c9c4dda2a2366f3f26f02e
Minervavr <minerva_veru@hotmail.com>
2016-01-09 18:51:29 -0430

    M16_ContratodeInterazparaEliminarItem
    se implementa el contrato de la interfaz para eliminar item del carrito

---
+++ Interfaz_Contratos/Modulo16/IContratoEliminarItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace Interfaz_Contratos.Modulo16
+{
+    public interface IContratoEliminarItem
+    {
+        /// <summary>
+        /// Tabla donde se mostraran todos los implementos que hay en el carrito del usuario
+        /// </summary>
+        Table tablaImplemento { get; }
+
+        /// <summary>
+        /// Tabla donde se mostraran todos los eventos que hay en el carrito del usuario
+        /// </summary>
+        Table tablaEvento { get; }
+
+        /// <summary>
+        /// Tabla donde se mostraran todas las matriculas que hay en el carrito del usuario
+        /// </summary>
+        Table tablaMatricula { get; }
+
+    }
+}
