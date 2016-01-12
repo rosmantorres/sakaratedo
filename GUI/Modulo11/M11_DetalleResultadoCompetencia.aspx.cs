3a0bb703302139121a6272588dfb1126256440f2
Andreinabetlo <Andreinabetlo@gmail.com>
2016-01-12 15:48:46 -0430

    M11_DetalleCompetencia
    Ventana detalle competencia

---
+++ GUI/Modulo11/M11_DetalleResultadoCompetencia.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace templateApp.GUI.Modulo11
+{
+    public partial class M11_DetalleResultadoCompetencia : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bCancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("M11_ListarResultadoCompetencia.aspx");
+        }
+    }
+}
