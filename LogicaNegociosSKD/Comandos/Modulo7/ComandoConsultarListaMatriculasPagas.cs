09aa394609f72af71d128648989d8451f5bb2d69
adrianabetta <adri1191@hotmail.com>
2016-01-15 17:05:10 -0430

    M7_ComandoLIstarMatriculasPagas
    Comando Listar Matriculas Pagas
    Pruebas unitaris Listar Matriculas Pagas

---
+++ LogicaNegociosSKD/Comandos/Modulo7/ComandoConsultarListaMatriculasPagas.cs
@@ -0,0 +1,81 @@
+﻿using DatosSKD.DAO.Modulo7;
+using DatosSKD.Fabrica;
+using DominioSKD;
+using ExcepcionesSKD;
+using ExcepcionesSKD.Modulo7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegociosSKD.Comandos.Modulo7
+{
+    public class ComandoConsultarListaMatriculasPagas : Comando<Tuple<List<Entidad>, List<Boolean>, List<float>>>
+    {
+        /// <summary>
+        /// Implementación del metodo abstracto Ejecutar de la clase comando
+        /// </summary>
+        /// <returns>Retorta tupla con listas de evento, competencia y listas de sus fechas de inscripción</returns>
+        public override Tuple<List<Entidad>, List<Boolean>, List<float>> Ejecutar()
+        {
+            FabricaDAOSqlServer fabrica = new FabricaDAOSqlServer();
+            DaoMatricula baseDeDatosMatricula = fabrica.ObtenerDaoMatriculaM7();        
+            List<Entidad> matriculas = new List<Entidad>();
+            Persona idPersona = (Persona)LaEntidad;
+            Tuple<List<Entidad> ,List<Boolean>, List<float>> tupla;
+            List<Boolean> listaEstado = new List<Boolean>();
+            List<float> listaMonto = new List<float>();           
+            try
+            {
+                if (idPersona.ID > 0)
+                {
+                    matriculas = baseDeDatosMatricula.ListarMatriculasPagas(idPersona);
+                   
+                    foreach (Matricula matricula in matriculas)
+                    {
+                        listaEstado.Add(baseDeDatosMatricula.EstadoMatricula(idPersona));
+                    }
+
+                    foreach (Matricula matricula in matriculas)
+                    {
+                        listaMonto.Add(baseDeDatosMatricula.MontoPagoMatricula(idPersona,matricula));
+                    }
+                
+
+                    tupla = Tuple.Create(matriculas, listaEstado, listaMonto);
+                }
+                else
+                {
+                    throw new NumeroEnteroInvalidoException(RecursosComandoModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosComandoModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+                }
+            }
+            catch (ExceptionSKDConexionBD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (NumeroEnteroInvalidoException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new NumeroEnteroInvalidoException(RecursosComandoModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosComandoModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+            }
+            catch (FormatException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new NumeroEnteroInvalidoException(RecursosComandoModulo7.Codigo_Numero_Parametro_Invalido,
+                                RecursosComandoModulo7.Mensaje_Numero_Parametro_invalido, new Exception());
+            }
+            catch (ExceptionSKD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            return tupla;
+        }
+    }
+}
---
+++ PruebasUnitariasSKD/Modulo7/PruebasComando/M7_PruebasComandoMatriculasPagas.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LogicaNegociosSKD.Fabrica;
+using DominioSKD;
+using LogicaNegociosSKD.Comandos.Modulo7;
+using DominioSKD.Fabrica;
+using ExcepcionesSKD.Modulo7;
+
+namespace PruebasUnitariasSKD.Modulo7.PruebasComando
+{
+    [TestFixture]
+    public class M7_PruebasComandoMatriculas
+    {
+        #region Atributos
+        private Persona idPersona;
+        private FabricaComandos fabricaComandos;
+        private ComandoConsultarListaMatriculasPagas matriculasPagas;
+        private FabricaEntidades fabricaEntidades;
+        #endregion
+
+        #region SetUp & TearDown
+        [SetUp]
+        public void Init()
+        {
+            fabricaComandos = new FabricaComandos();
+            matriculasPagas = (ComandoConsultarListaMatriculasPagas)fabricaComandos.ObtenerComandoConsultarListaMatriculasPagas();
+            fabricaEntidades = new FabricaEntidades();
+            idPersona = new Persona();//cambiar por fabrica
+            idPersona.ID = 6;
+            matriculasPagas.LaEntidad = idPersona;
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            fabricaComandos = null;
+            matriculasPagas = null;
+            fabricaEntidades = null;
+            idPersona = null;
+        }
+        #endregion
+
+        #region Test
+
+        /// <summary>
+        /// Método para probar que la tupla obtenida no es nula en eventos asistidos
+        /// </summary>
+        [Test]
+        public void PruebaObtenerTuplaMatriculasPagas()
+        {
+            Tuple<List<Entidad>, List<Boolean>, List<float>> tupla = matriculasPagas.Ejecutar();
+            Assert.IsNotNull(tupla);
+        }
+
+        /// <summary>
+        /// Método para probar la excepcion de número entero invalido en obtener lista eventos asistidos
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(NumeroEnteroInvalidoException))]
+        public void ListaMatriculasNumeroEnteroException()
+        {
+            idPersona.ID = -1;
+            Tuple<List<Entidad>, List<Boolean>, List<float>> tupla = matriculasPagas.Ejecutar();
+        }
+
+        /// <summary>
+        /// Método para probar que la lista obtenida de eventos puede tener uno o mas eventos
+        /// </summary>
+        [Test]
+        public void PruebaObtenerListaMatriculasPagas()
+        {
+            Tuple<List<Entidad>, List<Boolean>, List<float>> tupla = matriculasPagas.Ejecutar();
+            List<Entidad> listaEvento = tupla.Item1;
+            Assert.GreaterOrEqual(listaEvento.Count, 1);
+        }
+
+
+        /// <summary>
+        /// Método para probar que la lista obtenida de fechas sobre eventos puede tener uno o mas fechas
+        /// </summary>
+        [Test]
+        public void PruebaObtenerEstadoMatriculasPagas()
+        {
+            Tuple<List<Entidad>, List<Boolean>, List<float>> tupla = matriculasPagas.Ejecutar();
+            List<Boolean> listaEstadoMatricula = tupla.Item2;
+            Assert.GreaterOrEqual(listaEstadoMatricula.Count, 1);
+        }
+
+        /// <summary>
+        /// Método para probar que la lista obtenida de fechas sobre competencias puede tener uno o mas competencias
+        /// </summary>
+        [Test]
+        public void PruebaObtenerListaMontoMatriculasPagas()
+        {
+            Tuple<List<Entidad>, List<Boolean>, List<float>> tupla = matriculasPagas.Ejecutar();
+            List<float> listaMontoMatriculaPaga = tupla.Item3;
+            Assert.GreaterOrEqual(listaMontoMatriculaPaga.Count, 1);
+        }
+        #endregion
+    }
+}
