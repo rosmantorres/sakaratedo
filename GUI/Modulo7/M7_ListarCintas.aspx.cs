c40281554fe9aacf8bfb308afb175bfc2ac5a8a8
Pedro Villarreal <pedrovillarreal@gmail.com>
2016-01-14 20:17:07 -0430

    M7_MPVListarCintas
    MPV funcionando en listar cintas, falta agregarle la id por session,
    comentarios y excepciones.

--- GUI/Modulo7/M7_ListarCintas.aspx.cs
+++ GUI/Modulo7/M7_ListarCintas.aspx.cs
@@ -19,6 +19,7 @@ namespace templateApp.GUI.Modulo7
     {
         #region Atributos
         private List<Cinta> laLista = new List<Cinta>();
+        private PresentadorListarCintasObtenidas presentador;
 
         #region Contrato
         string IContratoListarCintasObtenidas.laTabla
@@ -37,6 +38,11 @@ namespace templateApp.GUI.Modulo7
 
         #endregion
 
+        public M7_ListarCintas()
+        {
+            presentador = new PresentadorListarCintasObtenidas(this);
+        }
+
         #region Page_Load
         /// <summary>
         /// Método que se ejecuta al cargar la página Listar Cintas
@@ -48,11 +54,10 @@ namespace templateApp.GUI.Modulo7
             ((SKD)Page.Master).IdModulo = "7";
 
             String detalleString = Request.QueryString["compDetalle"];
-            DateTime fechaInscripcion;
+                       
+        #region Llenar DataTable con Cintas
 
-            #region Llenar DataTable con Cintas
-
-            LogicaCintas logEvento = new LogicaCintas();
+        LogicaCintas logEvento = new LogicaCintas();
 
             try
             {
@@ -71,33 +76,7 @@ namespace templateApp.GUI.Modulo7
                     {
                         try
                         {
-
- 
-
-
-                            //laLista = logEvento.obtenerListaDeCintas(int.Parse(Session[RecursosInterfazMaster.sessionUsuarioID].ToString()));
-                            /*if (laLista != null)
-                            {
-                                foreach (Cinta cinta in laLista)
-                                {
-                                    fechaInscripcion = logEvento.obtenerFechaCinta(int.Parse(Session[RecursosInterfazMaster.sessionUsuarioID].ToString()), cinta.Id_cinta);
-                                    this.laTabla.Text += M7_Recursos.AbrirTR;
-                                    this.laTabla.Text += M7_Recursos.AbrirTD + cinta.Color_nombre.ToString() + M7_Recursos.CerrarTD;
-                                    this.laTabla.Text += M7_Recursos.AbrirTD + cinta.Rango.ToString() + M7_Recursos.CerrarTD;
-                                    this.laTabla.Text += M7_Recursos.AbrirTD + fechaInscripcion.ToString("MM/dd/yyyy") + M7_Recursos.CerrarTD;
-                                    this.laTabla.Text += M7_Recursos.AbrirTD + cinta.Clasificacion.ToString() + M7_Recursos.CerrarTD;
-                                    this.laTabla.Text += M7_Recursos.AbrirTD;
-                                    this.laTabla.Text += M7_Recursos.BotonInfoCintas + cinta.Id_cinta + M7_Recursos.BotonCerrar;
-                                    this.laTabla.Text += M7_Recursos.CerrarTD;
-                                    this.laTabla.Text += M7_Recursos.CerrarTR;
-                                }
-                            }
-                            else
-                            {
-                                throw new ListaNulaException(M7_Recursos.Codigo_Lista_Nula,
-                                M7_Recursos.Mensaje_Numero_Parametro_invalido, new Exception());
-                            }*/
-
+                            presentador.ConsultarCintasObtenidas();
                         }
                         catch (ListaNulaException)
                         {
--- Interfaz_Presentadores/Modulo7/PresentadorListarCintasObtenidas.cs
+++ Interfaz_Presentadores/Modulo7/PresentadorListarCintasObtenidas.cs
@@ -1,4 +1,8 @@
-﻿using Interfaz_Contratos.Modulo7;
+﻿using DominioSKD;
+using DominioSKD.Fabrica;
+using Interfaz_Contratos.Modulo7;
+using LogicaNegociosSKD;
+using LogicaNegociosSKD.Fabrica;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +21,69 @@ namespace Interfaz_Presentadores.Modulo7
         public PresentadorListarCintasObtenidas(IContratoListarCintasObtenidas laVista)
         {
             vista = laVista;
+        }
+
+        public void ConsultarCintasObtenidas()
+        {
+            FabricaComandos fabricaComandos = new FabricaComandos();
+            FabricaEntidades fabricaEntidades = new FabricaEntidades();
+            try
+            {
+                Comando<Tuple<List<Entidad>, List<DateTime>>> comandoListaCintasObtenidas = fabricaComandos.ObtenerComandoConsultarListaCinta();
+                Persona idPersona = new Persona();//cambiar por fabrica
+
+                //idPersona.ID = int.Parse(Session[RecursosInterfazMaster.sessionUsuarioID].ToString());
+                idPersona.ID = 6; //falta modificar esto
+                comandoListaCintasObtenidas.LaEntidad = idPersona;
+                Tuple<List<Entidad>, List<DateTime>> tupla = comandoListaCintasObtenidas.Ejecutar();
+
+                List<Entidad> listaCinta = tupla.Item1;
+                List<DateTime> listaFecha = tupla.Item2;
+
+                /*var numbersAndWords = listaCinta.Zip(listaFecha, (n, w) => new { cinta = n, fecha = w });
+
+                foreach (var nw in numbersAndWords)
+                {
+                    Cinta cinta = (Cinta)nw.cinta;
+                    DateTime fechaInscripcion = nw.fecha;
+       
+                    vista.laTabla += M7_RecursosPresentador.AbrirTR;
+                    vista.laTabla += M7_RecursosPresentador.AbrirTD + cinta.Color_nombre.ToString() + M7_RecursosPresentador.CerrarTD;
+                    vista.laTabla += M7_RecursosPresentador.AbrirTD + cinta.Rango.ToString() + M7_RecursosPresentador.CerrarTD;
+                    vista.laTabla += M7_RecursosPresentador.AbrirTD + fechaInscripcion.ToString("MM/dd/yyyy") + M7_RecursosPresentador.CerrarTD;
+                    vista.laTabla += M7_RecursosPresentador.AbrirTD + cinta.Clasificacion.ToString() + M7_RecursosPresentador.CerrarTD;
+                    vista.laTabla += M7_RecursosPresentador.AbrirTD;
+                    vista.laTabla += M7_RecursosPresentador.BotonInfoCintas + cinta.Id_cinta + M7_RecursosPresentador.BotonCerrar;
+                    vista.laTabla += M7_RecursosPresentador.CerrarTD;
+                    vista.laTabla += M7_RecursosPresentador.CerrarTR;
+                }*/
+
+                using (var e1 = listaCinta.GetEnumerator())
+                using (var e2 = listaFecha.GetEnumerator())
+                {
+                    while (e1.MoveNext() && e2.MoveNext())
+                    {
+                        Cinta cinta = (Cinta)e1.Current;
+                        DateTime fechaInscripcion = e2.Current;
+
+                        vista.laTabla += M7_RecursosPresentador.AbrirTR;
+                        vista.laTabla += M7_RecursosPresentador.AbrirTD + cinta.Color_nombre.ToString() + M7_RecursosPresentador.CerrarTD;
+                        vista.laTabla += M7_RecursosPresentador.AbrirTD + cinta.Rango.ToString() + M7_RecursosPresentador.CerrarTD;
+                        vista.laTabla += M7_RecursosPresentador.AbrirTD + fechaInscripcion.ToString("MM/dd/yyyy") + M7_RecursosPresentador.CerrarTD;
+                        vista.laTabla += M7_RecursosPresentador.AbrirTD + cinta.Clasificacion.ToString() + M7_RecursosPresentador.CerrarTD;
+                        vista.laTabla += M7_RecursosPresentador.AbrirTD;
+                        vista.laTabla += M7_RecursosPresentador.BotonInfoCintas + cinta.Id_cinta + M7_RecursosPresentador.BotonCerrar;
+                        vista.laTabla += M7_RecursosPresentador.CerrarTD;
+                        vista.laTabla += M7_RecursosPresentador.CerrarTR;
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
