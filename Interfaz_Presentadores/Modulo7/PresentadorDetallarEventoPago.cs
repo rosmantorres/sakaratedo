7e6229ace6214ea1bdbe14c6b900d0f4a3886d27
adrianabetta <adri1191@hotmail.com>
2016-01-17 13:43:09 -0430

    M7_IPresentadorDetallarEventoPagoYDetallarMatricula

---
+++ Interfaz_Presentadores/Modulo7/PresentadorDetallarEventoPago.cs
@@ -0,0 +1,83 @@
+﻿using DominioSKD;
+using ExcepcionesSKD;
+using ExcepcionesSKD.Modulo7;
+using Interfaz_Contratos.Modulo7;
+using LogicaNegociosSKD.Comandos.Modulo7;
+using LogicaNegociosSKD.Fabrica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaz_Presentadores.Modulo7
+{
+    /// <summary>
+    /// Presentador para detallar evento
+    /// </summary>
+    public class PresentadorDetallarEventoPago
+    {
+        private FabricaComandos fabricaComandos;
+        private IContratoDetallarEvento vista;
+        /// <summary>
+        /// Constructor del presentador
+        /// </summary>
+        /// <param name="laVista">instancia de la ventana</param>
+        public PresentadorDetallarEventoPago(IContratoDetallarEvento laVista)
+        {
+            vista = laVista;
+        }
+
+
+        /// <summary>
+        /// Método para cargar los datos del evento
+        /// </summary>
+        /// <param name="idEvento">id del evento</param>
+        public void cargarDatos(Evento idEvento)
+        {
+            try
+            {
+                fabricaComandos = new FabricaComandos();
+                ComandoConsultarDetallarEvento comandoDetallarEventoPago = (ComandoConsultarDetallarEvento)fabricaComandos.ObtenerComandoConsultarDetallarEventoPago();
+                comandoDetallarEventoPago.LaEntidad = idEvento;
+                Evento evento = (Evento)comandoDetallarEventoPago.Ejecutar();
+
+                vista.ciudad_evento = evento.Ubicacion.Ciudad;
+                vista.costo_evento = evento.Costo.ToString();
+                vista.descripcion_evento = evento.Descripcion;
+               // vista.direccionEvento_evento = evento.Ubicacion.Direccion;
+                vista.estadoUbicacion_evento = evento.Ubicacion.Estado;
+                if (evento.Estado.Equals(true))
+                {
+                    vista.estado_evento = M7_RecursosPresentador.AliasEventoActivo;
+                }
+                else if (evento.Estado.Equals(false))
+                {
+                    vista.estado_evento = M7_RecursosPresentador.AliasEventoInactivo;
+                }
+                vista.fechaFin_evento = evento.Horario.FechaFin.ToString("MM/dd/yyyy");
+                vista.fechaInicio_evento = evento.Horario.FechaInicio.ToString("MM/dd/yyyy");
+                vista.horaFin_evento = evento.Horario.HoraFin.ToString();
+                vista.horaInicio_evento = evento.Horario.HoraInicio.ToString();
+                vista.nombre_evento = evento.Nombre;
+                             
+            }
+            catch (NumeroEnteroInvalidoException)
+            {
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+    M7_RecursosPresentador.Mensaje_Numero_Parametro_invalido, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            catch (FormatException)
+            {
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+    M7_RecursosPresentador.Mensaje_Numero_Parametro_invalido, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            catch (NullReferenceException)
+            {
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Interfaz_Presentadores/Modulo7/PresentadorDetallarMatricula.cs
@@ -0,0 +1,81 @@
+﻿using DominioSKD;
+using DominioSKD.Fabrica;
+using ExcepcionesSKD;
+using ExcepcionesSKD.Modulo7;
+using Interfaz_Contratos.Modulo7;
+using LogicaNegociosSKD;
+using LogicaNegociosSKD.Comandos.Modulo7;
+using LogicaNegociosSKD.Fabrica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaz_Presentadores.Modulo7
+{
+    /// <summary>
+    /// Presentador para detallar matricula
+    /// </summary>
+    public class PresentadorDetallarMatricula
+    {
+        private FabricaComandos fabricaComandos;
+        private IContratoDetallarMatricula vista;
+        /// <summary>
+        /// Constructor del presentador
+        /// </summary>
+        /// <param name="laVista">instancia de la ventana</param>
+        public PresentadorDetallarMatricula(IContratoDetallarMatricula laVista)
+        {
+            vista = laVista;
+        }
+
+
+        /// <summary>
+        /// Método para cargar los datos de la matricula
+        /// </summary>
+        /// <param name="idCinta">id de la matricula</param>
+        /// <param name="idPersona">id de la persona</param>
+        public void cargarDatos(Matricula idMatricula)
+        {           
+            try
+            {
+               fabricaComandos = new FabricaComandos();
+                ComandoConsultarDetallarMatricula comandoDetallarMatricula =(ComandoConsultarDetallarMatricula)fabricaComandos.ObtenerComandoConsultarDetallarMatricula();
+                comandoDetallarMatricula.LaEntidad = idMatricula;
+               // comandoDetallarMatricula. = idPersona;
+                Matricula matricula = (Matricula)comandoDetallarMatricula.Ejecutar();
+                String estadoFinal;
+                vista.identificadorMatricula = matricula.Identificador;
+                vista.fechaCreacionMatricula = matricula.FechaCreacion.ToString("MM/dd/yyyy");
+                vista.fechaPagoMatricula = matricula.UltimaFechaPago.ToString("MM/dd/yyyy");
+                estadoFinal = matricula.Status.ToString();
+
+                if (matricula.Status.Equals(true))
+                {
+                    estadoFinal = "Activa";
+                }
+                else
+                {
+                    estadoFinal = "No Activa";
+                }
+            }
+            catch (NumeroEnteroInvalidoException)
+            {
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+    M7_RecursosPresentador.Mensaje_Numero_Parametro_invalido, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            catch (FormatException)
+            {
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+    M7_RecursosPresentador.Mensaje_Numero_Parametro_invalido, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            catch (NullReferenceException)
+            {               
+            }
+            catch (Exception)
+            {               
+            }
+        }
+    }
+}
