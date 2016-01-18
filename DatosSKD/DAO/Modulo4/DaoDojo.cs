88e56ab730701a81fdde20d680658a64fdf57222
jessikad <jessikadaboin1@gmail.com>
2016-01-18 16:57:16 -0430

    M4

---
+++ DatosSKD/DAO/Modulo4/DaoDojo.cs
@@ -0,0 +1,78 @@
+﻿using DatosSKD.DAO;
+using DatosSKD.InterfazDAO;
+using DominioSKD;
+using DominioSKD.Fabrica;
+using ExcepcionesSKD;
+using ExcepcionesSKD.Modulo4;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosSKD.DAO.Modulo4
+{
+    class DaoDojo : DAOGeneral
+    {
+        #region IDAO
+        public bool Agregar(Entidad parametro)
+        {
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosDAOModulo4.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            try
+            {
+                DominioSKD.Entidades.Modulo4 = (DominioSKD.Entidades.Modulo4.Dojo)parametro;
+
+                if (!BuscarNombreCompetenciaAgregar(laCompetencia))
+                {
+                }
+                else
+                {
+                    Logger.EscribirWarning(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosDAOModulo4.Mensaje_Dojo_Existente, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+                    throw new ExcepcionesSKD.Modulo4.DojoInexistenteException(RecursosDAOModulo4.Codigo_Dojo_Existente,
+                                RecursosDAOModulo4.Mensaje_Dojo_Existente, new Exception());
+                }
+            }
+            catch (SqlException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw new ExcepcionesSKD.ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (FormatException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw new ExcepcionesSKD.Modulo4.FormatoIncorrectoException(RecursosDAOModulo4.Codigo_Error_Formato,
+                     RecursosDAOModulo4.Mensaje_Error_Formato, ex);
+            }
+            catch (ExcepcionesSKD.Modulo4.DojoInexistenteException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw new ExcepcionesSKD.ExceptionSKD(RecursoGeneralBD.Mensaje_Generico_Error, ex);
+            }
+
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosDAOModulo4.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ DominioSKD/Entidades/Modulo4/Dojo.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioSKD.Entidades.Modulo4
+{
+    class Dojo : Entidad
+    {
+        #region atributos
+
+        private int id_dojo;
+        private String rif_dojo;
+        private String nombre_dojo;
+        private int telefono_dojo;
+        private String email_dojo;
+        private String logo_dojo;
+        private DateTime registro_dojo;
+        private bool status_dojo;
+        private Organizacion organizacion;
+        private Ubicacion ubicacion;
+        private string estilo;
+        #endregion
+
+        #region propiedades
+
+        public int Id_dojo
+        {
+            get { return id_dojo; }
+            set { id_dojo = value; }
+        }
+
+        public String Rif_dojo
+        {
+            get { return rif_dojo; }
+            set { rif_dojo = value; }
+        }
+        public String Nombre_dojo
+        {
+            get { return nombre_dojo; }
+            set { nombre_dojo = value; }
+        }
+        public int Telefono_dojo
+        {
+            get { return telefono_dojo; }
+            set { telefono_dojo = value; }
+        }
+        public String Email_dojo
+        {
+            get { return email_dojo; }
+            set { email_dojo = value; }
+        }
+        public String Logo_dojo
+        {
+            get { return logo_dojo; }
+            set { logo_dojo = value; }
+        }
+        public DateTime Registro_dojo
+        {
+            get { return registro_dojo; }
+
+            set { registro_dojo = value; }
+        }
+        public bool Status_dojo
+        {
+            get { return status_dojo; }
+            set { status_dojo = value; }
+        }
+        public Organizacion Organizacion
+        {
+            get { return organizacion; }
+            set { organizacion = value; }
+        }
+        public Ubicacion Ubicacion
+        {
+            get { return ubicacion; }
+            set { ubicacion = value; }
+        }
+        public String Estilo_dojo
+        {
+            get { return estilo; }
+            set { estilo = value; }
+        }
+        #endregion
+
+        #region constructores
+
+        //constructor vacio
+        public Dojo()
+        {
+            this.id_dojo = 0;
+            this.rif_dojo = "";
+            this.nombre_dojo = "";
+            this.telefono_dojo = 0;
+            this.email_dojo = "";
+            this.logo_dojo = "";
+            this.status_dojo = false;
+            this.organizacion = null;
+            this.ubicacion = null;
+            this.estilo = "";
+        }
+
+        //constructor con parametros 
+        public Dojo(int Id, string Rif, string Nombre, int Telefono, string Email, string Logo, bool Status, Organizacion organizacion, Ubicacion ubicacion)
+        {
+            this.id_dojo = Id;
+            this.rif_dojo = Rif;
+            this.nombre_dojo = Nombre;
+            this.telefono_dojo = Telefono;
+            this.email_dojo = Email;
+            this.logo_dojo = Logo;
+            this.status_dojo = Status;
+            this.organizacion = organizacion;
+            this.ubicacion = ubicacion;
+        }
+        //constructor sin id 
+        public Dojo( string Rif, string Nombre, int Telefono, string Email, string Logo, bool Status, Organizacion organizacion, Ubicacion ubicacion)
+        {
+            this.rif_dojo = Rif;
+            this.nombre_dojo = Nombre;
+            this.telefono_dojo = Telefono;
+            this.email_dojo = Email;
+            this.logo_dojo = Logo;
+            this.status_dojo = Status;
+            this.organizacion = organizacion;
+            this.ubicacion = ubicacion;
+        }
+        #endregion
+    }
+}
