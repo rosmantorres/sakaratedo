f0ca5bf38884fbc8983a3cde6786d71b1a6edc28
Javier Perez <gperez10@gmail.com>
2016-01-18 20:20:44 -0430

    Dao crear Evento

--- DatosSKD/DAO/Modulo9/DaoEvento.cs
+++ DatosSKD/DAO/Modulo9/DaoEvento.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using DominioSKD;
 using ExcepcionesSKD;
 using ExcepcionesSKD.Modulo12;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 
 
@@ -18,53 +21,54 @@ namespace DatosSKD.DAO.Modulo9
         /// </summary>
         /// <param name="evento">El Evento a Agregar</param>
         /// <returns>Retorna true o false</returns>
-        public bool CrearEvento(Entidad evento)
+        public bool CrearEvento(Entidad eventoParametro)
         {
-            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosBDModulo9.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosDaoEvento.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
 
             try
             {
                 //parametros para insertar un evento
-               
+                DominioSKD.Entidades.Modulo9.Evento evento = (DominioSKD.Entidades.Modulo9.Evento)eventoParametro;
+
                 List<Parametro> parametros = new List<Parametro>();
-                Parametro parametro = new Parametro(RecursosBDModulo9.ParametroNombreEvento, SqlDbType.VarChar, evento.Nombre, false);
+                Parametro parametro = new Parametro(RecursosDaoEvento.ParametroNombreEvento, SqlDbType.VarChar, evento.Nombre, false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroDescripcionEvento, SqlDbType.VarChar, evento.Descripcion, false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroDescripcionEvento, SqlDbType.VarChar, evento.Descripcion, false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroCostoEvento, SqlDbType.Float, evento.Costo.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroCostoEvento, SqlDbType.Float, evento.Costo.ToString(), false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroEstadoEvento, SqlDbType.VarChar, evento.Estado.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroEstadoEvento, SqlDbType.VarChar, evento.Estado.ToString(), false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroIdPersona, SqlDbType.Int, evento.Persona.ID.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroIdPersona, SqlDbType.Int, evento.Persona.ID.ToString(), false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroIdUbicacion, SqlDbType.Int,"1", false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroIdUbicacion, SqlDbType.Int,"1", false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroIdTipoEvento, SqlDbType.Int, evento.TipoEvento.Id.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroIdTipoEvento, SqlDbType.Int, evento.TipoEvento.Id.ToString(), false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroIdCategoria, SqlDbType.Int, "1", false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroIdCategoria, SqlDbType.Int, "1", false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroFechaInicio, SqlDbType.Date, evento.Horario.FechaInicio.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroFechaInicio, SqlDbType.Date, evento.Horario.FechaInicio.ToString(), false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroFechaFin, SqlDbType.Date, evento.Horario.FechaFin.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroFechaFin, SqlDbType.Date, evento.Horario.FechaFin.ToString(), false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroHoraInicio, SqlDbType.Int, evento.Horario.HoraInicio.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroHoraInicio, SqlDbType.Int, evento.Horario.HoraInicio.ToString(), false);
                 parametros.Add(parametro);
-                parametro = new Parametro(RecursosBDModulo9.ParametroHoraFin, SqlDbType.Int, evento.Horario.HoraFin.ToString(), false);
+                parametro = new Parametro(RecursosDaoEvento.ParametroHoraFin, SqlDbType.Int, evento.Horario.HoraFin.ToString(), false);
                 parametros.Add(parametro);
 
                 BDConexion con = new BDConexion();
-                List<Resultado> resultados = con.EjecutarStoredProcedure(RecursosBDModulo9.ProcedimientoAgregarEvento, parametros);
+                List<Resultado> resultados = con.EjecutarStoredProcedure(RecursosDaoEvento.ProcedimientoAgregarEvento, parametros);
 
                 //si la creacion es correcta retorna true
 
                 if (resultados != null)
                 {
-                    Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosBDModulo9.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+                    Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosDaoEvento.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
                     return true;
                 }
                 else
                 {
-                    Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosBDModulo9.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+                    Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosDaoEvento.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
                     return false;
 
                 }
@@ -80,8 +84,8 @@ namespace DatosSKD.DAO.Modulo9
             {
                 Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
 
-                throw new ExcepcionesSKD.Modulo12.FormatoIncorrectoException(RecursosBDModulo9.CodigoErrorFormato,
-                     RecursosBDModulo9.MensajeErrorFormato, ex);
+                throw new ExcepcionesSKD.Modulo12.FormatoIncorrectoException(RecursosDaoEvento.CodigoErrorFormato,
+                     RecursosDaoEvento.MensajeErrorFormato, ex);
             }
             catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
             {
--- DatosSKD/DAO/Modulo9/RecursosDaoEvento.Designer.cs
+++ DatosSKD/DAO/Modulo9/RecursosDaoEvento.Designer.cs
@@ -59,5 +59,365 @@ namespace DatosSKD.DAO.Modulo9 {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to costoEvento.
+        /// </summary>
+        internal static string AliasCostoEvento {
+            get {
+                return ResourceManager.GetString("AliasCostoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to descripcionEvento.
+        /// </summary>
+        internal static string AliasDescripcionEvento {
+            get {
+                return ResourceManager.GetString("AliasDescripcionEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to estadoEvento.
+        /// </summary>
+        internal static string AliasEstadoEvento {
+            get {
+                return ResourceManager.GetString("AliasEstadoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to fechaFin.
+        /// </summary>
+        internal static string AliasFechaFin {
+            get {
+                return ResourceManager.GetString("AliasFechaFin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to fechaInicio.
+        /// </summary>
+        internal static string AliasFechaInicio {
+            get {
+                return ResourceManager.GetString("AliasFechaInicio", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to horaFin.
+        /// </summary>
+        internal static string AliasHoraFin {
+            get {
+                return ResourceManager.GetString("AliasHoraFin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to horaInicio.
+        /// </summary>
+        internal static string AliasHoraInicio {
+            get {
+                return ResourceManager.GetString("AliasHoraInicio", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to idEvento.
+        /// </summary>
+        internal static string AliasIdEvento {
+            get {
+                return ResourceManager.GetString("AliasIdEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to idTipo.
+        /// </summary>
+        internal static string AliasIDTipoEvento {
+            get {
+                return ResourceManager.GetString("AliasIDTipoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to nombreEvento.
+        /// </summary>
+        internal static string AliasNombreEvento {
+            get {
+                return ResourceManager.GetString("AliasNombreEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to tipoEvento.
+        /// </summary>
+        internal static string AliasTipoEvento {
+            get {
+                return ResourceManager.GetString("AliasTipoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to SKD_00_004.
+        /// </summary>
+        internal static string CodigoErrorFormato {
+            get {
+                return ResourceManager.GetString("CodigoErrorFormato", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Error de parseo.
+        /// </summary>
+        internal static string MensajeErrorFormato {
+            get {
+                return ResourceManager.GetString("MensajeErrorFormato", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Salida del Método.
+        /// </summary>
+        internal static string MensajeFinInfoLogger {
+            get {
+                return ResourceManager.GetString("MensajeFinInfoLogger", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Entrada en el Método.
+        /// </summary>
+        internal static string MensajeInicioInfoLogger {
+            get {
+                return ResourceManager.GetString("MensajeInicioInfoLogger", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @costo.
+        /// </summary>
+        internal static string ParametroCostoEvento {
+            get {
+                return ResourceManager.GetString("ParametroCostoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @descripcion.
+        /// </summary>
+        internal static string ParametroDescripcionEvento {
+            get {
+                return ResourceManager.GetString("ParametroDescripcionEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @estado.
+        /// </summary>
+        internal static string ParametroEstadoEvento {
+            get {
+                return ResourceManager.GetString("ParametroEstadoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @fechaFin.
+        /// </summary>
+        internal static string ParametroFechaFin {
+            get {
+                return ResourceManager.GetString("ParametroFechaFin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @fechaInicio.
+        /// </summary>
+        internal static string ParametroFechaInicio {
+            get {
+                return ResourceManager.GetString("ParametroFechaInicio", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @horaFin.
+        /// </summary>
+        internal static string ParametroHoraFin {
+            get {
+                return ResourceManager.GetString("ParametroHoraFin", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @horaInicio.
+        /// </summary>
+        internal static string ParametroHoraInicio {
+            get {
+                return ResourceManager.GetString("ParametroHoraInicio", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @idCategoria.
+        /// </summary>
+        internal static string ParametroIdCategoria {
+            get {
+                return ResourceManager.GetString("ParametroIdCategoria", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @idEvento.
+        /// </summary>
+        internal static string ParametroIdEvento {
+            get {
+                return ResourceManager.GetString("ParametroIdEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @idPersona.
+        /// </summary>
+        internal static string ParametroIdPersona {
+            get {
+                return ResourceManager.GetString("ParametroIdPersona", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @idTipoEvento.
+        /// </summary>
+        internal static string ParametroIdTipoEvento {
+            get {
+                return ResourceManager.GetString("ParametroIdTipoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @idUbicacion.
+        /// </summary>
+        internal static string ParametroIdUbicacion {
+            get {
+                return ResourceManager.GetString("ParametroIdUbicacion", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @nombre.
+        /// </summary>
+        internal static string ParametroNombreEvento {
+            get {
+                return ResourceManager.GetString("ParametroNombreEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to @nombreTipoEvento.
+        /// </summary>
+        internal static string ParametroNombreTipoEvento {
+            get {
+                return ResourceManager.GetString("ParametroNombreTipoEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_ConsultarEventoXID.
+        /// </summary>
+        internal static string ProcedimentoConsultarEventoXID {
+            get {
+                return ResourceManager.GetString("ProcedimentoConsultarEventoXID", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_AgregarEventoCompleto.
+        /// </summary>
+        internal static string ProcedimientoAgregarEvento {
+            get {
+                return ResourceManager.GetString("ProcedimientoAgregarEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_AgregarEventoConTipo.
+        /// </summary>
+        internal static string ProcedimientoAgregarEventoConTipo {
+            get {
+                return ResourceManager.GetString("ProcedimientoAgregarEventoConTipo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_ConsultarAscensosRangoFecha.
+        /// </summary>
+        internal static string ProcedimientoConsultarAscensosRangoFecha {
+            get {
+                return ResourceManager.GetString("ProcedimientoConsultarAscensosRangoFecha", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_ConsultarEventos.
+        /// </summary>
+        internal static string ProcedimientoConsultarEventos {
+            get {
+                return ResourceManager.GetString("ProcedimientoConsultarEventos", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_ConsultarEventosRangoFecha.
+        /// </summary>
+        internal static string ProcedimientoConsultarEventosRangoFecha {
+            get {
+                return ResourceManager.GetString("ProcedimientoConsultarEventosRangoFecha", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_TodasLasFechasAscensos.
+        /// </summary>
+        internal static string ProcedimientoConsultarFechasAscensos {
+            get {
+                return ResourceManager.GetString("ProcedimientoConsultarFechasAscensos", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_TodasLasFechas.
+        /// </summary>
+        internal static string ProcedimientoConsultarHorarios {
+            get {
+                return ResourceManager.GetString("ProcedimientoConsultarHorarios", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_TodosLosTiposEvento.
+        /// </summary>
+        internal static string ProcedimientoConsultarTiposEventos {
+            get {
+                return ResourceManager.GetString("ProcedimientoConsultarTiposEventos", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_ModificarEvento.
+        /// </summary>
+        internal static string ProcedimientoModificarEvento {
+            get {
+                return ResourceManager.GetString("ProcedimientoModificarEvento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to M9_ModificarEventoConTipo.
+        /// </summary>
+        internal static string ProcedimientoModificarEventoConTipo {
+            get {
+                return ResourceManager.GetString("ProcedimientoModificarEventoConTipo", resourceCulture);
+            }
+        }
     }
 }
