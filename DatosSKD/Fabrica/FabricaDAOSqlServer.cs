72992c8a158623db5cc3ffec256ed46b3f881baf
Manuel28G <man281192@gmail.com>
2016-01-20 02:21:27 -0430

    M1M2_FabricaDAO

--- DatosSKD/Fabrica/FabricaDAOSqlServer.cs
+++ DatosSKD/Fabrica/FabricaDAOSqlServer.cs
@@ -16,9 +16,21 @@ namespace DatosSKD.Fabrica
     public class FabricaDAOSqlServer
     {
         #region Modulo 1
+        public InterfazDAO.Modulo1.IDaoLogin ObtenerDaoLogin()
+        {
+            return new DAO.Modulo1.DaoLogin();
+        }
+        public InterfazDAO.Modulo1.IDaoRestablecer ObtenerDaoRestablecer()
+        {
+            return new DAO.Modulo1.DaoRestablecer();
+        }
         #endregion
 
         #region Modulo 2
+        public InterfazDAO.Modulo2.IDaoRoles ObtenerDaoRoles()
+        {
+            return new DAO.Modulo2.DaoRoles();
+        }
         #endregion
 
         #region Modulo 3
