bf177f92ff426f3ceb103c481ad1c73a9ab83e38
mgraterol <mgratero@gmail.com>
2016-01-23 00:23:11 -0430

    Comando Agregar Evento
    Sin implementacion

---
+++ LogicaNegociosSKD/Comandos/Modulo9/ComandoAgregarEvento.cs
@@ -0,0 +1,24 @@
+﻿using DatosSKD.Fabrica;
+using DatosSKD.InterfazDAO.Modulo9;
+using DominioSKD;
+using ExcepcionesSKD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegociosSKD.Comandos.Modulo9
+{
+    public class ComandoAgregarEvento : Comando<bool>
+    {
+        public ComandoAgregarEvento(Entidad entidad)
+        {
+            LaEntidad = entidad;
+        }
+        public override bool Ejecutar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LogicaNegociosSKD/Comandos/Modulo9/ComandoAgregarEventoConTipo.cs
@@ -0,0 +1,25 @@
+﻿using DatosSKD.Fabrica;
+using DatosSKD.InterfazDAO.Modulo9;
+using DominioSKD;
+using ExcepcionesSKD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LogicaNegociosSKD.Comandos.Modulo9
+{
+    public class ComandoAgregarEventoConTipo : Comando<bool>
+    {
+        public ComandoAgregarEventoConTipo(Entidad entidad)
+        {
+            LaEntidad = entidad;
+        }
+        public override bool Ejecutar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
