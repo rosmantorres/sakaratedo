68cc96f5a04725e277356f1361504e4c89f522c9
ieba <istvanbokor8@gmail.com>
2016-01-23 12:14:07 -0430

    dao RestEvento
    modificaciones de restriccion evento en dao

--- DatosSKD/DAO/Modulo8/DAORestriccionEvento.cs
+++ DatosSKD/DAO/Modulo8/DAORestriccionEvento.cs
@@ -39,8 +39,8 @@ namespace DatosSKD.DAO.Modulo8
         /// <summary>
         /// Metodo para agregar una restriccion de evento a la base de datos.
         /// </summary>
-        /// <param name="parametro"></param>
-        /// <returns></returns>
+        /// <param name="parametro">objeto de tipo RestriccionEvento para agregar en bd</param>
+        /// <returns>true si fue eliminado</returns>
         public Boolean AgregarRestriccionEvento(DominioSKD.Entidad parametro)
         {
             DominioSKD.Entidades.Modulo8.RestriccionEvento laRestriccionEvento =
@@ -103,8 +103,8 @@ namespace DatosSKD.DAO.Modulo8
         /// <summary>
         /// Metodo para modificar una restriccion de evento a la base de datos.
         /// </summary>
-        /// <param name="parametro"> objeto RestriccionEvento</param>
-        /// <returns></returns>
+        /// <param name="parametro"> objeto RestriccionEvento para modificar en bd</param>
+        /// <returns>true si fue modificado</returns>
         public Boolean ModificarRestriccionEvento(DominioSKD.Entidad parametro)
         {
             DominioSKD.Entidades.Modulo8.RestriccionEvento laRestriccionEvento =
@@ -170,18 +170,17 @@ namespace DatosSKD.DAO.Modulo8
         /// <summary>
         /// Metodo para eliminar una restriccion de evento en la base de datos.
         /// </summary>
-        /// <param name="parametro"> objeto RestriccionEvento</param>
-        /// <returns></returns>
-        public Boolean EliminarRestriccionEvento(DominioSKD.Entidad parametro)
+        /// <param name="parametro"> int id de la RestriccioEvento para ser eliminado en bd</param>
+        /// <returns>true si fue eliminado</returns>
+        public Boolean EliminarRestriccionEvento(int parametro)
         {
-            DominioSKD.Entidades.Modulo8.RestriccionEvento laRestriccionEvento =
-                (DominioSKD.Entidades.Modulo8.RestriccionEvento)parametro;
+            int idRestriccionEvento = parametro;
             try
             {
                 List<Parametro> parametros = new List<Parametro>(); //declaras lista de parametros
 
                 Parametro elParametro = new Parametro(RecursosDAORestriccionEvento.ParamIdRestriccionEvento, SqlDbType.Int,
-                    laRestriccionEvento.ToString(), false);
+                    idRestriccionEvento.ToString(), false);
                 //parametro recibe: el alias de la accion (en este caso es la descripcion de mi restriccion de cinta que apunta a un atributo que se llama @DescripcionRestriccionCinta), SqlDbType es el tipo de dato que tiene ese atributo en la base de datos (en este caso es varchar), el elemento que se desea poner en ese lugar (aqui se usa la clase dominio), el false lo dejas asi
                 parametros.Add(elParametro);
                 //agregas eso que acabas de hacer a la lista de parametros.
@@ -218,7 +217,107 @@ namespace DatosSKD.DAO.Modulo8
         }
         #endregion
 
-        
+        #region AgregarRH_Cinta
+        /// <summary>
+        /// Metodo para agregar las cintas de una RestriccionEvento en la base de datos.
+        /// </summary>
+        /// <param name="parametro1"> objeto RestriccionEvento para agregar su id en RH_Cinta</param>
+        /// <param name="parametro2"> int del Id de la Cinta para ser agregado su id a la RestriccionEvento</param>
+        /// <returns>true si fue agregado</returns>
+        public Boolean AgregarRh_Cinta(DominioSKD.Entidad parametro1, int parametro2)
+        {
+            DominioSKD.Entidades.Modulo8.RestriccionEvento laRestriccionEvento =
+                (DominioSKD.Entidades.Modulo8.RestriccionEvento)parametro1;
+
+            int elIdCinta = parametro2;
+
+            try
+            {
+                List<Parametro> parametros = new List<Parametro>(); //declaras lista de parametros
+
+                Parametro elParametro = new Parametro(RecursosDAORestriccionEvento.ParamIdCinta, SqlDbType.Int,
+                    elIdCinta.ToString(), false);
+                //parametro recibe: el alias de la accion (en este caso es la descripcion de mi restriccion de cinta que apunta a un atributo que se llama @DescripcionRestriccionCinta), SqlDbType es el tipo de dato que tiene ese atributo en la base de datos (en este caso es varchar), el elemento que se desea poner en ese lugar (aqui se usa la clase dominio), el false lo dejas asi
+                parametros.Add(elParametro);
+                //agregas eso que acabas de hacer a la lista de parametros.
+                //repites hasta que tengas todos los parametros de tu stored procedure asociado
+                elParametro = new Parametro(RecursosDAORestriccionEvento.ParamIdRestriccionEvento, SqlDbType.Int,
+                    laRestriccionEvento.IdRestEvento.ToString(), false);
+                parametros.Add(elParametro);
+
+                BDConexion laConexion = new BDConexion();// abres la conexion
+                laConexion.EjecutarStoredProcedure(RecursosDAORestriccionEvento.AgregarRH_Cinta
+                                             , parametros);//ejecutas el stored procedure que quieres pasandole la lista de parametros
+            }
+
+            catch (SqlException ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesSKD.ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesSKD.ExceptionSKD(RecursoGeneralBD.Mensaje_Generico_Error, ex);
+            }
+
+
+            return true;
+        }
+        #endregion
+
+        #region EliminarRH_Cinta
+        /// <summary>
+        /// Metodo para eliminar las cintas de RestriccionEvento en la base de datos en RH_Cinta.
+        /// </summary>
+        /// <param name="parametro"> objeto RestriccionEvento para eliminar sus cintas relacionadas en bd</param>
+        /// <returns>true si fue eliminado</returns>
+        public Boolean EliminarRh_Cinta(DominioSKD.Entidad parametro)
+        {
+            DominioSKD.Entidades.Modulo8.RestriccionEvento laRestriccionEvento =
+                (DominioSKD.Entidades.Modulo8.RestriccionEvento)parametro;
+
+            try
+            {
+                List<Parametro> parametros = new List<Parametro>(); //declaras lista de parametros
+
+                Parametro elParametro = new Parametro(RecursosDAORestriccionEvento.ParamIdRestriccionEvento, SqlDbType.Int,
+                    laRestriccionEvento.IdRestEvento.ToString(), false);
+                //parametro recibe: el alias de la accion (en este caso es la descripcion de mi restriccion de cinta que apunta a un atributo que se llama @DescripcionRestriccionCinta), SqlDbType es el tipo de dato que tiene ese atributo en la base de datos (en este caso es varchar), el elemento que se desea poner en ese lugar (aqui se usa la clase dominio), el false lo dejas asi
+                parametros.Add(elParametro);
+                //agregas eso que acabas de hacer a la lista de parametros.
+                //repites hasta que tengas todos los parametros de tu stored procedure asociado
+
+                BDConexion laConexion = new BDConexion();// abres la conexion
+                laConexion.EjecutarStoredProcedure(RecursosDAORestriccionEvento.EliminarRHCinta
+                                             , parametros);
+            }
+
+            catch (SqlException ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesSKD.ExceptionSKDConexionBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                //Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesSKD.ExceptionSKD(RecursoGeneralBD.Mensaje_Generico_Error, ex);
+            }
+            return true;
+        }
+        #endregion
 
     }
 }
