3e1c4bb4496c3c0ae01c10ec54ad71b89db8649e
mgraterol <mgratero@gmail.com>
2016-01-23 00:45:30 -0430

    Comando Agregar Implementado

--- LogicaNegociosSKD/Comandos/Modulo9/ComandoAgregarEvento.cs
+++ LogicaNegociosSKD/Comandos/Modulo9/ComandoAgregarEvento.cs
@@ -10,15 +10,48 @@ using System.Threading.Tasks;
 
 namespace LogicaNegociosSKD.Comandos.Modulo9
 {
+    /// <summary>
+    /// Comando que crea un evento
+    /// </summary>
+    /// <param name="Entidad">El evento a crear</param>
+    /// <returns>Verdadero o Falso</returns>
     public class ComandoAgregarEvento : Comando<bool>
     {
+
         public ComandoAgregarEvento(Entidad entidad)
         {
             LaEntidad = entidad;
         }
         public override bool Ejecutar()
         {
-            throw new NotImplementedException();
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosComandosModulo9.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            try
+            {
+                DatosSKD.DAO.Modulo9.DaoEvento daoEvento = (DatosSKD.DAO.Modulo9.DaoEvento)FabricaDAOSqlServer.ObtenerDaoEvento();
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, RecursosComandosModulo9.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+                return daoEvento.Agregar(LaEntidad);
+            }
+            catch (ExcepcionesSKD.ExceptionSKDConexionBD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.Modulo12.FormatoIncorrectoException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (ExcepcionesSKD.ExceptionSKD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+           
+
         }
     }
 }
