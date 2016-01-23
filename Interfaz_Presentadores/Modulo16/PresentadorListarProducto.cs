203051ae53db4e42ba91f03a9b16424b3933f600
tjsearsz <tjsearszu@gmail.com>
2016-01-23 00:58:32 -0430

    M16_AgregarCarrito
    Se corrige un detalle al seleccionar una celda que no es la invalida
    debido a que se agrego una celda nueva llamada "dojo"

--- Interfaz_Presentadores/Modulo16/PresentadorListarProducto.cs
+++ Interfaz_Presentadores/Modulo16/PresentadorListarProducto.cs
@@ -364,16 +364,16 @@ namespace Interfaz_Presentadores.Modulo16
                     //Si la fila no es de tipo Header puedo comenzar a buscar
                     if ((aux2 is TableHeaderRow) != true)
                     {
-                        //En la celda 7 siempre estaran los botones, casteo el boton
-                        Button aux3 = aux2.Cells[7].Controls[1] as Button;
+                        //En la celda 8 siempre estaran los botones, casteo el boton
+                        Button aux3 = aux2.Cells[8].Controls[1] as Button;
 
                         //Si el ID del boton en la fila actual corresponde con el ID del boton que realizo la accion
                         //Obtenemos el numero del textbox que el usuario desea
                         if (aux3.ID == aux.ID)
                         {
-                            /*En la celda 6 siempre estara el combobox,
+                            /*En la celda 7 siempre estara el combobox,
                             lo obtengo y agarro la cantidad que el usuario desea*/
-                            DropDownList lacantidad = aux2.Cells[6].Controls[0] as DropDownList;
+                            DropDownList lacantidad = aux2.Cells[7].Controls[0] as DropDownList;
                             cantidad = int.Parse(lacantidad.SelectedValue);
                             break;
                         }
