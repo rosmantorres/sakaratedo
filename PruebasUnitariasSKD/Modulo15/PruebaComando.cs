354453116fd3adcd3766f4871cff4ebf6b1a5672
Jose Daniel Andara Giannotti <josedaniel.ag1991@gmail.com>
2016-01-24 13:50:17 -0430

    M15_PruebasUnitarias
    Pruebas unitarias de DAO y Comando.

---
+++ PruebasUnitariasSKD/Modulo15/PruebaComando.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using LogicaNegociosSKD.Comandos.Modulo15;
+using DominioSKD;
+using DominioSKD.Entidades.Modulo15;
+namespace PruebasUnitariasSKD.Modulo15
+{
+    [TestFixture]
+    class PruebaComando
+    {
+        private Implemento implemento;
+        private Implemento implemento2;
+        private Dojo dojo;
+        ComandoAgregarImplemento cai;
+        ComandoEliminarImplemento cei;
+        ComandoConsultarTodosImplementos ccti;
+        ComandoConsultarTodosImplementos2 ccti2;
+        ComandoDojoId cdi;
+        ComandoImplementoXId cixi;
+        ComandoModificarImplemento cmi;
+        ComandoUsuarioDojo cud;
+        private List<Entidad> lista;
+        private List<Entidad> lista2;
+        private Usuario usuario;
+        int numero;
+        int numero2;
+        [SetUp]
+        public void Init()
+        {
+            cai = new ComandoAgregarImplemento();
+            cei = new ComandoEliminarImplemento();
+            ccti = new ComandoConsultarTodosImplementos();
+            ccti2 = new ComandoConsultarTodosImplementos2();
+            cmi = new ComandoModificarImplemento();
+            cixi = new ComandoImplementoXId();
+            cud = new ComandoUsuarioDojo();
+            implemento = new Implemento();
+            implemento2 = new Implemento();
+            usuario = new Usuario(1, "Rosman", "Torres");
+            dojo = new Dojo(1);
+            implemento.Nombre_Implemento = "Guantes Karate-DO";
+            implemento.Tipo_Implemento = "Guantes";
+            implemento.Marca_Implemento = "Adidas";
+            implemento.Color_Implemento = "Azul";
+            implemento.Talla_Implemento = "S";
+            implemento.Estatus_Implemento = "Activo";
+            implemento.Precio_Implemento = 1500;
+            implemento.Stock_Minimo_Implemento = 5;
+            implemento.Cantida_implemento = 20;
+            implemento.Dojo_Implemento = dojo;
+            implemento.Descripcion_Implemento = "Guantes Wola";
+            implemento.Imagen_implemento = "A.jpg";
+            cai.LaEntidad = implemento;
+            cei.LaEntidad = implemento;
+            ccti.LaEntidad = dojo;
+            ccti2.LaEntidad = dojo;
+            cmi.LaEntidad = implemento;
+            cixi.LaEntidad = implemento;
+            cud.LaEntidad = usuario;
+        }
+        [TearDown]
+        public void Reset()
+        {
+            implemento = null;
+            dojo = null;
+            lista = null;
+            lista2 = null;
+        }
+
+        #region M15_ComandoAgregarImplemento
+        [Test]
+        public void M15_ComandoAgregarImplemento()
+        {
+            Assert.True(cai.Ejecutar());
+
+        }
+        #endregion
+
+        #region M15_ComandoConsultarTodosImplementos
+        [Test]
+        public void M15_ComandoConsultarTodosImplementos()
+        {
+            cai.Ejecutar();
+            lista = ccti.Ejecutar();
+            lista2 = ccti.Ejecutar();
+            Assert.AreEqual(lista.Count(), lista2.Count());
+        }
+        #endregion
+
+        #region M15_ComandoConsultarTodosImplementos2
+        [Test]
+        public void M15_ComandoConsultarTodosImplementos2()
+        {
+            cai.Ejecutar();
+            lista = ccti2.Ejecutar();
+            lista2 = ccti2.Ejecutar();
+            Assert.AreEqual(lista.Count(), lista2.Count());
+
+        }
+        #endregion
+
+        #region M15_ComandoEliminarImplemento
+        [Test]
+        public void M15_ComandoEliminarImplemento()
+        {
+            cai.Ejecutar();
+            Assert.True(cei.Ejecutar());
+        }
+        #endregion
+
+        #region M15_ComandoModificarImplemento
+        [Test]
+        public void M15_ComandoModificarImplemento()
+        {
+            cai.Ejecutar();
+            Assert.True(cmi.Ejecutar());
+
+        }
+        #endregion
+    }
+}
---
+++ PruebasUnitariasSKD/Modulo15/PruebaDao.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using DominioSKD;
+using DominioSKD.Entidades.Modulo15;
+using DatosSKD.Fabrica;
+using ExcepcionesSKD.Modulo15.ExcepcionDao;
+namespace PruebasUnitariasSKD.Modulo15
+{
+    class PruebaDAO
+    {
+        private Implemento implemento;
+        private Dojo dojo;
+        private Implemento implemento2;
+        private Implemento implemento3;
+        private List<Entidad> lista;
+        private List<Entidad> lista2;
+        private String usuario;
+        [SetUp]
+        public void Init()
+        {
+            implemento = new Implemento();
+            implemento2 = new Implemento();
+            implemento3 = new Implemento();
+            dojo = new Dojo(1);
+            implemento.Nombre_Implemento = "Guantes Karate-DO";
+            implemento.Tipo_Implemento = "Guantes";
+            implemento.Marca_Implemento = "Adidas";
+            implemento.Color_Implemento = "Azul";
+            implemento.Talla_Implemento = "S";
+            implemento.Estatus_Implemento = "Activo";
+            implemento.Precio_Implemento = 1500;
+            implemento.Stock_Minimo_Implemento = 5;
+            implemento.Cantida_implemento = 20;
+            implemento.Dojo_Implemento = dojo;
+            implemento.Descripcion_Implemento = "Guantes Wola";
+            implemento.Imagen_implemento = "A.jpg";
+
+            implemento3.Nombre_Implemento = "Karategi Karate-DO";
+            implemento3.Tipo_Implemento = "Karategi";
+            implemento3.Marca_Implemento = "Kombate";
+            implemento3.Color_Implemento = "Verde";
+            implemento3.Talla_Implemento = "M";
+            implemento3.Estatus_Implemento = "Activo";
+            implemento3.Precio_Implemento = 1200;
+            implemento3.Stock_Minimo_Implemento = 4;
+            implemento3.Cantida_implemento = 10;
+            implemento3.Dojo_Implemento = dojo;
+            implemento3.Descripcion_Implemento = "Karategi Wololo";
+            implemento3.Imagen_implemento = "B.jpg";
+
+        }
+        [TearDown]
+        public void Reset()
+        {
+            implemento = null;
+            implemento2 = null;
+            lista = null;
+            lista2 = null;
+            dojo = null;
+        }
+        /*       #region M15_PruebaAgregarInventarioNulo
+               [Test]
+               [ExpectedException(typeof(ErrorEnParametroDeProcedure))]
+               public void M15_PruebaAgregarInventarioNulo()
+               {
+                  FabricaDAOSqlServer.ObtenerDAOImplemento().listarInventarioDatos(null);
+           
+               }
+               #endregion 
+
+               #region M15_PruebaAgregarInventarioAtributoNulo
+               #endregion
+               #region M15_PruebaAgregarInventarioAtributoStockNegativo
+               #endregion
+               #region egion M15_PruebaAgregarInventarioAtributoCantidadNegativo
+               #endregion
+               #region M15_PruebaAgregarInventarioAtributoPrecioNegativo
+               #endregion
+         */
+
+        #region M15_PruebaAgregarInventario
+        [Test]
+        public void M15_PruebaAgregarInventarioDatos()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().Agregar(implemento);
+            implemento2 = (Implemento)FabricaDAOSqlServer.ObtenerDAOImplemento().implementoInventarioDatosUltimo();
+            Assert.AreEqual(implemento.Nombre_Implemento, implemento2.Nombre_Implemento);
+            Assert.AreEqual(implemento.Tipo_Implemento, implemento2.Tipo_Implemento);
+            Assert.AreEqual(implemento.Marca_Implemento, implemento2.Marca_Implemento);
+            Assert.AreEqual(implemento.Color_Implemento, implemento2.Color_Implemento);
+            Assert.AreEqual(implemento.Talla_Implemento, implemento2.Talla_Implemento);
+            Assert.AreEqual(implemento.Precio_Implemento, implemento2.Precio_Implemento);
+            Assert.AreEqual(implemento.Cantida_implemento, implemento2.Cantida_implemento);
+            Assert.AreEqual("Activo", implemento2.Estatus_Implemento);
+            Assert.AreEqual(implemento.Stock_Minimo_Implemento, implemento2.Stock_Minimo_Implemento);
+            Assert.AreEqual(implemento.Imagen_implemento, implemento2.Imagen_implemento);
+        }
+        #endregion
+
+        #region M15_PruebaModificarInventario
+        [Test]
+        public void M15_PruebaModificarInventarioDatos()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().Agregar(implemento);
+            implemento.Nombre_Implemento = "Carton";
+            FabricaDAOSqlServer.ObtenerDAOImplemento().modificarInventarioDatos(implemento);
+            Assert.AreEqual(implemento.Nombre_Implemento, "Carton");
+            Assert.AreEqual(implemento.Tipo_Implemento, implemento.Tipo_Implemento);
+            Assert.AreEqual(implemento.Marca_Implemento, implemento.Marca_Implemento);
+            Assert.AreEqual(implemento.Color_Implemento, implemento.Color_Implemento);
+            Assert.AreEqual(implemento.Talla_Implemento, implemento.Talla_Implemento);
+            Assert.AreEqual(implemento.Precio_Implemento, implemento.Precio_Implemento);
+            Assert.AreEqual(implemento.Cantida_implemento, implemento.Cantida_implemento);
+            Assert.AreEqual(implemento.Estatus_Implemento, implemento.Estatus_Implemento);
+            Assert.AreEqual(implemento.Stock_Minimo_Implemento, implemento.Stock_Minimo_Implemento);
+            Assert.AreEqual(implemento.Imagen_implemento, implemento.Imagen_implemento);
+        }
+        #endregion
+        #region M15_PruebaModificarInventarioNulo
+        [Test]
+        [ExpectedException(typeof(ExcepcionesSKD.ExceptionSKD))]
+        public void M15_PruebaModificarInventarioNulo()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().modificarInventarioDatos(null);
+        }
+        #endregion
+        #region M15_PruebaModificarInventarioNulo
+        [Test]
+        [ExpectedException(typeof(ExcepcionmodificarInventarioDatos))]
+        public void M15_PruebaModificarInventarioParametroNulo()
+        {
+            implemento.Nombre_Implemento = null;
+            FabricaDAOSqlServer.ObtenerDAOImplemento().modificarInventarioDatos(implemento);
+        }
+        #endregion
+        #region M15_PruebaModificarInventarioNulo
+        [Test]
+        [ExpectedException(typeof(ExcepcionesSKD.ExceptionSKD))]
+        public void M15_PruebaModificarInventarioParametroNegativo()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().modificarInventarioDatos(null);
+        }
+        #endregion
+        #region M15_PruebaEliminarInventario
+        [Test]
+        public void M15_PruebaEliminarInventarioDatos()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().Agregar(implemento3);
+            FabricaDAOSqlServer.ObtenerDAOImplemento().eliminarInventarioDatos(implemento3.Id_Implemento, dojo);
+            implemento2 = (Implemento)FabricaDAOSqlServer.ObtenerDAOImplemento().implementoInventarioDatosUltimo();
+            Assert.AreNotEqual(implemento.Marca_Implemento, implemento2.Marca_Implemento);
+            Assert.AreNotEqual(implemento.Color_Implemento, implemento2.Color_Implemento);
+            Assert.AreNotEqual(implemento.Talla_Implemento, implemento2.Talla_Implemento);
+            Assert.AreNotEqual(implemento.Precio_Implemento, implemento2.Precio_Implemento);
+            Assert.AreNotEqual(implemento.Cantida_implemento, implemento2.Cantida_implemento);
+            Assert.AreNotEqual("Inactivo", implemento2.Estatus_Implemento);
+            Assert.AreNotEqual(implemento.Stock_Minimo_Implemento, implemento2.Stock_Minimo_Implemento);
+            Assert.AreNotEqual(implemento.Imagen_implemento, implemento2.Imagen_implemento);
+        }
+        #endregion
+
+        #region M15_ListarInventarioDatos()
+        [Test]
+        public void M15_ListarInventarioDatos()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().Agregar(implemento);
+            lista = FabricaDAOSqlServer.ObtenerDAOImplemento().listarInventarioDatos(dojo);
+            lista2 = FabricaDAOSqlServer.ObtenerDAOImplemento().listarInventarioDatos(dojo);
+            Assert.AreEqual(lista.Count(), lista2.Count());
+        }
+        #endregion
+
+        #region M15_ListaInventarioDatosNulo()
+        [Test]
+        [ExpectedException(typeof(ExcepcionesSKD.ExceptionSKD))]
+        public void M15_ListaInventarioDatosNulo()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().listarInventarioDatos(null);
+
+        }
+        #endregion
+
+        #region M15_ListarInventarioDatos2()
+        [Test]
+        public void M15_ListarInventarioDatos2()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().Agregar(implemento);
+            lista = FabricaDAOSqlServer.ObtenerDAOImplemento().listarInventarioDatos2(dojo);
+            lista2 = FabricaDAOSqlServer.ObtenerDAOImplemento().listarInventarioDatos2(dojo);
+            Assert.AreEqual(lista.Count(), lista2.Count());
+        }
+        #endregion
+
+        #region M15_ListaInventarioDatos2Nulo()
+        [Test]
+        [ExpectedException(typeof(ExcepcionesSKD.ExceptionSKD))]
+        public void M15_ListarInventarioDatos2Nulo()
+        {
+            FabricaDAOSqlServer.ObtenerDAOImplemento().listarInventarioDatos2(null);
+
+        }
+        #endregion
+    }
+}
